2e8490fdcc95a97206f1c5a8c93b12624ad0d18e
AndPotap <apotapczynski@gmail.com>
2020-01-14 21:25:59 -0500

    Took out unsued flag

--- vae_experiments/mnist_vae.py
+++ vae_experiments/mnist_vae.py
@@ -17,12 +17,12 @@ hyper.update({'latent_discrete_n': hyper['n_required']})
 
 if model_type in ['GS', 'ExpGS', 'GSDis', 'ExpGSDis']:
     hyper.update({'model_type': model_type, 'temp': 0.15, 'num_of_discrete_param': 1,
-                  'use_analytical_in_test': False, 'run_analytical_kl': False})
+                  'run_closed_form_kl': False})
 
 elif model_type in ['IGR_I_Dis', 'IGR_I']:
     hyper.update({'model_type': model_type, 'temp': 0.10,
                   'prior_file': './Results/mu_xi_unif_10_ng.pkl', 'num_of_discrete_param': 2,
-                  'use_analytical_in_test': True, 'run_analytical_kl': True})
+                  'run_closed_form_kl': True})
 else:
     raise RuntimeError
 run_vae(hyper=hyper, run_with_sample=run_with_sample)
