e44fff2a035ab24e26ee4a4171230e14671234c2
AndPotap <apotapczynski@gmail.com>
2020-01-23 16:42:53 -0500

    Added the change in architecture for CelebA in Planar Exp

--- vae_experiments/grid.py
+++ vae_experiments/grid.py
@@ -1,28 +1,26 @@
 import tensorflow as tf
 from Models.train_vae import run_vae
 run_with_sample = True
-# model_type = 'GSM'
-# model_type = 'ExpGS'
-model_type = 'SB'
+model_type = 'IGR_Planar_Dis'
 
-hyper = {'dataset_name': 'fmnist', 'sample_size': 1, 'n_required': 10, 'num_of_discrete_var': 1,
-         'latent_norm_n': 10, 'num_of_norm_var': 1, 'num_of_norm_param': 2,
-         'architecture': 'conv_jointvae', 'learning_rate': 0.0005, 'batch_n': 64, 'epochs': 100,
-         'run_ccβvae': True, 'γ': tf.constant(30.),
-         'continuous_c_linspace': (0., 5., 25_000), 'discrete_c_linspace': (0., 5., 25_000)}
-hyper.update({'latent_discrete_n': hyper['n_required']})
+hyper = {'dataset_name': 'mnist', 'sample_size': 1, 'n_required': 9, 'num_of_discrete_var': 30,
+         'latent_norm_n': 0, 'num_of_norm_var': 0, 'num_of_norm_param': 0,
+         'learning_rate': 0.001, 'batch_n': 64, 'epochs': 100, 'architecture': 'dense',
+         'run_jv': False, 'γ': tf.constant(30.),
+         'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
+hyper.update({'latent_discrete_n': hyper['n_required'] + 1})
+hyper.update({'model_type': model_type, 'temp': 0.10,
+              'prior_file': './Results/mu_xi_unif_10_IGR_SB_Finite.pkl', 'num_of_discrete_param': 2,
+              'run_closed_form_kl': True})
 
-
-experiment = {1: {'model_type': 'ExpGS', 'temp': 0.15, 'run_analytical_kl': False,
-                  'num_of_discrete_param': 1, 'use_analytical_in_test': False},
-              2: {'model_type': 'GSM', 'temp': 0.10, 'run_analytical_kl': True,
-                  'num_of_discrete_param': 2, 'use_analytical_in_test': True,
-                  'prior_file': './Results/mu_xi_unif_10_ng.pkl'},
-              3: {'latent_discrete_n': 50, 'threshold': 0.99, 'temp': 0.1,
-                  'model_type': 'SB', 'num_of_discrete_param': 2, 'truncation_option': 'quantile',
-                  'prior_file': './Results/mu_xi_unif_50_sb.pkl',
-                  'use_analytical_in_test': True, 'run_analytical_kl': True}
-              }
+experiment = {
+    1: {'dataset_name': 'mnist', 'temp': 0.10},
+    2: {'dataset_name': 'mnist', 'temp': 0.50},
+    3: {'dataset_name': 'fmnist', 'temp': 0.10},
+    4: {'dataset_name': 'fmnist', 'temp': 0.50},
+    5: {'dataset_name': 'celeb_a', 'temp': 0.10, 'architecture': 'conv_jointvae'},
+    6: {'dataset_name': 'celeb_a', 'temp': 0.10, 'architecture': 'conv_jointvae'},
+}
 
 for _, d in experiment.items():
     for key, value in d.items():
