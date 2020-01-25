a915544a087fb910aa92fa9edb1adc7eab49b995
AndPotap <apotapczynski@gmail.com>
2020-01-25 17:43:20 -0500

    Using namedtuple for cases

--- vae_experiments/grid.py
+++ vae_experiments/grid.py
@@ -1,26 +1,36 @@
 import tensorflow as tf
+from collections import namedtuple
 from Models.train_vae import run_vae
 run_with_sample = True
 model_type = 'IGR_Planar_Dis'
+epochs = 50
+n_required = 9
+latent_discrete_n = n_required + 1
 
-hyper = {'dataset_name': 'mnist', 'sample_size': 1, 'n_required': 9, 'num_of_discrete_var': 30,
+hyper = {'dataset_name': 'mnist', 'sample_size': 1, 'n_required': n_required, 'num_of_discrete_var': 30,
          'latent_norm_n': 0, 'num_of_norm_var': 0, 'num_of_norm_param': 0,
-         'learning_rate': 0.001, 'batch_n': 64, 'epochs': 100, 'architecture': 'dense',
+         'learning_rate': 0.001, 'batch_n': 64, 'epochs': epochs, 'architecture': 'dense',
          'run_jv': False, 'γ': tf.constant(30.),
          'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
-hyper.update({'latent_discrete_n': hyper['n_required'] + 1})
+hyper.update({'latent_discrete_n': latent_discrete_n})
 hyper.update({'model_type': model_type, 'temp': 0.10,
               'prior_file': './Results/mu_xi_unif_10_IGR_SB_Finite.pkl', 'num_of_discrete_param': 2,
               'run_closed_form_kl': True})
 
-datasets = ['mnist', 'fmnist']
-temps = [0.25, 0.4, 0.67, 0.85, 1.]
+Case = namedtuple('Case', 'dataset arch')
+datasets = [Case(dataset='mnist', arch='dense'),
+            Case(dataset='fmnist', arch='dense'),
+            Case(dataset='celeb_a', arch='conv_jointvae')]
+temps = [0.1, 0.25, 0.5]
+# temps = [0.67, 0.85, 1.0]
 i = 0
 experiment = {}
 for d in datasets:
     for t in temps:
         i += 1
-        experiment.update({i: {'dataset_name': d, 'temp': t}})
+        exp = {'dataset_name': d.dataset, 'architecture': d.arch, 'temp': t}
+        print(exp)
+        experiment.update({i: exp})
 
 for _, d in experiment.items():
     for key, value in d.items():
