b5df773248f9b11f4ada340c7074934a99878b3f
AndPotap <apotapczynski@gmail.com>
2020-01-24 21:11:43 -0500

    Added Discrete and Continuous Planar Flow

--- Models/train_vae.py
+++ Models/train_vae.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from Utils.load_data import load_vae_dataset
 from Models.VAENet import construct_networks, determine_path_to_save_results
 from Models.OptVAE import OptVAE, OptIGR, OptSB, OptSBDis, OptExpGS
-from Models.OptVAE import OptIGRDis, OptExpGSDis, OptPlanarNFDis
+from Models.OptVAE import OptIGRDis, OptExpGSDis, OptPlanarNFDis, OptPlanarNF
 from Utils.viz_vae import plot_originals, plot_reconstructions_samples_and_traversals
 from Utils.general import setup_logger, append_timestamp_to_file
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -44,6 +44,8 @@ def construct_nets_and_optimizer(hyper, model_type):
         vae_opt = OptSBDis(nets=nets, optimizer=optimizer, hyper=hyper)
     elif model_type == 'IGR_Planar_Dis':
         vae_opt = OptPlanarNFDis(nets=nets, optimizer=optimizer, hyper=hyper)
+    elif model_type == 'IGR_Planar':
+        vae_opt = OptPlanarNF(nets=nets, optimizer=optimizer, hyper=hyper)
     else:
         raise RuntimeError
     return vae_opt
