4d229a22c5606ad5c3ae9e28cf64a5692e999f33
AndPotap <apotapczynski@gmail.com>
2020-01-24 22:53:34 -0500

    Added all the hyperparameters for SB CelebA

--- vae_experiments/celeb_a_jv.py
+++ vae_experiments/celeb_a_jv.py
@@ -5,9 +5,10 @@
 import tensorflow as tf
 from Models.train_vae import run_vae
 run_with_sample = True
-model_type = 'IGR_Planar'
+# model_type = 'IGR_Planar'
+model_type = 'IGR_SB'
 
-hyper = {'dataset_name': 'celeb_a', 'sample_size': 1, 'n_required': 9, 'num_of_discrete_var': 1,
+hyper = {'dataset_name': 'celeb_a', 'sample_size': 1, 'n_required': 49, 'num_of_discrete_var': 1,
          'latent_norm_n': 32, 'num_of_norm_var': 1, 'num_of_norm_param': 2,
          'learning_rate': 0.001, 'batch_n': 64, 'epochs': 100, 'architecture': 'conv_jointvae',
          'run_jv': True, 'γ': tf.constant(100.),
@@ -21,6 +22,10 @@ if model_type in ['GS', 'GS_Dis']:
 elif model_type in ['IGR_I', 'IGR_Planar']:
     hyper.update({'model_type': model_type, 'temp': 0.50, 'num_of_discrete_param': 2,
                   'run_closed_form_kl': True})
+elif model_type in ['IGR_SB', 'IGR_SB_Finite']:
+    hyper.update({'model_type': model_type, 'temp': 0.50, 'num_of_discrete_param': 2,
+                  'run_closed_form_kl': True, 'threshold': 0.99, 'truncation_option': 'quantile',
+                  'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl'})
 else:
     raise RuntimeError
 run_vae(hyper=hyper, run_with_sample=run_with_sample)
