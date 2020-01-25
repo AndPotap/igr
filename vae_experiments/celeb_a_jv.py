e39d62e911f7d8052c460ac3e11706848d5f3361
AndPotap <apotapczynski@gmail.com>
2020-01-24 21:12:09 -0500

    Fixed legacy hyper names

--- vae_experiments/celeb_a_jv.py
+++ vae_experiments/celeb_a_jv.py
@@ -10,8 +10,8 @@ model_type = 'IGR_Planar'
 hyper = {'dataset_name': 'celeb_a', 'sample_size': 1, 'n_required': 9, 'num_of_discrete_var': 1,
          'latent_norm_n': 32, 'num_of_norm_var': 1, 'num_of_norm_param': 2,
          'learning_rate': 0.001, 'batch_n': 64, 'epochs': 100, 'architecture': 'conv_jointvae',
-         'run_ccβvae': True, 'γ': tf.constant(100.),
-         'continuous_c_linspace': (0., 50., 100_000), 'discrete_c_linspace': (0., 10., 100_000)}
+         'run_jv': True, 'γ': tf.constant(100.),
+         'cont_c_linspace': (0., 50., 100_000), 'disc_c_linspace': (0., 10., 100_000)}
 hyper.update({'latent_discrete_n': hyper['n_required'] + 1})
 
 if model_type in ['GS', 'GS_Dis', 'GSDis', 'ExpGSDis']:
