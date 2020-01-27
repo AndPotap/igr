c900ebdce531e3b9ae5b8bd1fbfa2ce1a31eb8f5
AndPotap <apotapczynski@gmail.com>
2020-01-26 21:01:56 -0500

    Added cases for JointVAE run

--- vae_experiments/grid.py
+++ vae_experiments/grid.py
@@ -1,42 +1,46 @@
 import tensorflow as tf
-from collections import namedtuple
 from Models.train_vae import run_vae
 run_with_sample = True
-epochs = 50
-model_type = 'IGR_Planar_Dis'
+epochs = 5
+model_type = 'IGR_Planar'
 n_required = 9
+# n_required = 49
 latent_discrete_n = n_required + 1
 run_closed_form_kl = True
-# model_type = 'GS_Dis'
+num_of_discrete_param = 2
+# model_type = 'GS'
 # n_required = 10
 # latent_discrete_n = n_required
 # run_closed_form_kl = False
+# num_of_discrete_param = 1
 
 temps = [0.1, 0.25, 0.5]
 # temps = [0.67, 0.85, 1.0]
 
-hyper = {'dataset_name': 'mnist', 'sample_size': 1, 'n_required': n_required, 'num_of_discrete_var': 30,
-         'latent_norm_n': 0, 'num_of_norm_var': 0, 'num_of_norm_param': 0,
-         'learning_rate': 0.001, 'batch_n': 64, 'epochs': epochs, 'architecture': 'dense',
-         'run_jv': False, 'γ': tf.constant(30.),
-         'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
-hyper.update({'latent_discrete_n': latent_discrete_n})
-hyper.update({'model_type': model_type, 'temp': 0.10,
-              'prior_file': './Results/mu_xi_unif_10_IGR_SB_Finite.pkl', 'num_of_discrete_param': 2,
-              'run_closed_form_kl': run_closed_form_kl})
+hyper = {'model_type': model_type, 'architecture': 'conv_jointvae',
+         'n_required': n_required, 'latent_discrete_n': latent_discrete_n,
+         'num_of_discrete_param': num_of_discrete_param, 'num_of_discrete_var': 1,
+         'num_of_norm_param': 2, 'num_of_norm_var': 1,
+         'epochs': epochs, 'learning_rate': 0.0005, 'batch_n': 64, 'sample_size': 1,
+         'run_jv': True, 'run_closed_form_kl': run_closed_form_kl,
+         'prior_file': './Results/mu_xi_unif_49_IGR_SB_Finite.pkl'}
 
-Case = namedtuple('Case', 'dataset arch')
-datasets = [Case(dataset='mnist', arch='dense'),
-            Case(dataset='fmnist', arch='dense'),
-            Case(dataset='celeb_a', arch='conv_jointvae')]
+cases = {
+    1: {'dataset_name': 'mnist', 'γ': tf.constant(30.), 'latent_norm_n': 10,
+        'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)},
+    2: {'dataset_name': 'fmnist', 'γ': tf.constant(30.), 'latent_norm_n': 10,
+        'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)},
+    3: {'dataset_name': 'celeb_a', 'γ': tf.constant(100.), 'latent_norm_n': 32,
+        'cont_c_linspace': (0., 50., 100_000), 'disc_c_linspace': (0., 10., 100_000)}
+}
 i = 0
 experiment = {}
-for d in datasets:
+for _, c in cases.items():
     for t in temps:
         i += 1
-        exp = {'dataset_name': d.dataset, 'architecture': d.arch, 'temp': t}
-        print(exp)
-        experiment.update({i: exp})
+        c.update({'temp': t})
+        print(c)
+        experiment.update({i: c})
 
 for _, d in experiment.items():
     for key, value in d.items():
