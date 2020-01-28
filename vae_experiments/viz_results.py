ec456da98488ad19557ea9b8c749eff636562481
AndPotap <apotapczynski@gmail.com>
2020-01-28 08:41:17 -0500

    Moved legend to the side

--- vae_experiments/viz_results.py
+++ vae_experiments/viz_results.py
@@ -52,9 +52,10 @@ for _, model in models.items():
                            color=model['color'], linestyle=linestyle, offset=offset)
 
 plt.xlabel('Epochs')
-plt.legend()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 6})
+# plt.tight_layout(rect=(0, 0, 0.95, 1))
+plt.tight_layout()
 # plt.savefig('./Results/Outputs/jv_comp.png')
 plt.savefig('./Results/Outputs/elbo.eps', format='eps')
-plt.tight_layout()
 plt.show()
 # ===========================================================================================================
