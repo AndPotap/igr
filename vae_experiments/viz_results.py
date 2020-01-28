2a48dba57ad9de994f386a508fa83c83465ec60b
AndPotap <apotapczynski@gmail.com>
2020-01-27 19:46:04 -0500

    Uses dict to plot results

--- vae_experiments/viz_results.py
+++ vae_experiments/viz_results.py
@@ -14,26 +14,24 @@ from Utils.general import make_np_of_var_from_log_files, add_mean_std_plot_line
 # gs = make_np_of_var_from_log_files(variable_name='TeJV ',
 #                                    files_list=[file for file in listdir(path = path_to_files)],
 #                                    path_to_files=path_to_files)
-
-path_to_files = './Results/elbo/mnist/gs_high/'
-gs_high = make_np_of_var_from_log_files(variable_name='TeELBO ',
-                                        files_list=[file for file in listdir(path=path_to_files)],
-                                        path_to_files=path_to_files)
-
-path_to_files = './Results/elbo/mnist/gs_low/'
-gs_low = make_np_of_var_from_log_files(variable_name='TeELBO ',
-                                       files_list=[file for file in listdir(path=path_to_files)],
-                                       path_to_files=path_to_files)
-
-path_to_files = './Results/elbo/mnist/igr_high/'
-igr_high = make_np_of_var_from_log_files(variable_name='TeELBOC',
-                                         files_list=[file for file in listdir(path=path_to_files)],
-                                         path_to_files=path_to_files)
-
-path_to_files = './Results/elbo/mnist/igr_low/'
-igr_low = make_np_of_var_from_log_files(variable_name='TeELBOC',
-                                        files_list=[file for file in listdir(path=path_to_files)],
-                                        path_to_files=path_to_files)
+path = './Results/elbo/mnist/'
+models = {
+    1: {'model': 'igr_planar_cv', 'label': 'IGR_Planar(0.85)', 'color': '#e41a1c'},
+    2: {'model': 'igr_planar_20', 'label': 'IGR_Planar(0.1)', 'color': '#e41a1c'},
+    3: {'model': 'igr_sb_cv', 'label': 'IGR_SB(0.1)', 'color': '#377eb8'},
+    4: {'model': 'igr_sb_20', 'label': 'IGR_SB(0.03)', 'color': '#377eb8'},
+    5: {'model': 'igr_i_cv', 'label': 'IGR_I(1.0)', 'color': '#4daf4a'},
+    6: {'model': 'igr_i_20', 'label': 'IGR_I(0.1)', 'color': '#4daf4a'},
+    7: {'model': 'gs_cv', 'label': 'GS(1.0)', 'color': '#984ea3'},
+    8: {'model': 'gs_20', 'label': 'GS(0.25)', 'color': '#984ea3'},
+}
+for key, val in models.items():
+    dirs = path + val['model'] + '/'
+    variable_name = 'TeELBOC' if val['model'].find('igr') > 0 else 'TeELBO'
+    output = make_np_of_var_from_log_files(variable_name=variable_name,
+                                           files_list=[file for file in listdir(path=dirs)],
+                                           path_to_files=dirs)
+    models[key]['results'] = output
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # ===========================================================================================================
 # Plots
@@ -48,10 +46,10 @@ offset = 5
 # plt.ylabel('Test Joint VAE loss')
 
 plt.ylabel('Test ELBO')
-add_mean_std_plot_line(runs=igr_high, label='IGR(0.50)', color='blue', offset=offset)
-add_mean_std_plot_line(runs=gs_high, label='GS(0.67)', color='green', offset=offset)
-add_mean_std_plot_line(runs=igr_low, label='IGR(0.1)', color='blue', offset=offset, linestyle='--')
-add_mean_std_plot_line(runs=gs_low, label='GS(0.25)', color='green', offset=offset, linestyle='--')
+for _, model in models.items():
+    linestyle = '-' if model['model'].find('cv') > 0 else '--'
+    add_mean_std_plot_line(runs=model['results'], label=model['label'],
+                           color=model['color'], linestyle=linestyle, offset=offset)
 
 plt.xlabel('Epochs')
 plt.legend()
