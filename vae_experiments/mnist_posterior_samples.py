8af3943d08ce81d58a71cdc87cea2524b68b43cd
AndPotap <apotapczynski@gmail.com>
2020-01-30 20:24:12 -0500

    Added CelebA sampling

--- vae_experiments/mnist_posterior_samples.py
+++ vae_experiments/mnist_posterior_samples.py
@@ -4,8 +4,8 @@ from Utils.posterior_sample_funcs import sample_from_posterior
 import seaborn as sns
 import pandas as pd
 
-dataset_name = 'mnist'
-run_with_sample = False
+dataset_name = 'celeb_a'
+run_with_sample = True
 plots_path = './Results/Outputs/'
 path = './Results/Current_Model/' + dataset_name + '/'
 models = {
@@ -13,9 +13,9 @@ models = {
     # 3: {'model': 'igr_sb_cv', 'label': 'IGR_SB(0.1)', 'type': 'IGR_SB_Dis'},
     # 5: {'model': 'igr_i_cv', 'label': 'IGR_I(1.0)', 'type': 'IGR_I_Dis'},
     # 7: {'model': 'gs_cv', 'label': 'GS(1.0)', 'type': 'GS_Dis'},
-    # 2: {'model': 'igr_planar_20', 'label': 'IGR_Planar(0.1)', 'type': 'IGR_Planar_Dis'},
+    2: {'model': 'igr_planar_20', 'label': 'IGR_Planar(0.1)', 'type': 'IGR_Planar_Dis'},
     4: {'model': 'igr_sb_20', 'label': 'IGR_SB(0.03)', 'type': 'IGR_SB_Dis'},
-    # 6: {'model': 'igr_i_20', 'label': 'IGR_I(0.1)', 'type': 'IGR_I_Dis'},
+    6: {'model': 'igr_i_20', 'label': 'IGR_I(0.1)', 'type': 'IGR_I_Dis'},
     8: {'model': 'gs_20', 'label': 'GS(0.25)', 'type': 'GS_Dis'},
 }
 for key, val in models.items():
