48c7a109ccfeb71ba28e9cec28a76ae49fea015a
AndPotap <apotapczynski@gmail.com>
2020-01-31 15:16:08 -0500

    Improved cv grid hyperparameter creation

--- vae_experiments/cv_grid.py
+++ vae_experiments/cv_grid.py
@@ -1,48 +1,56 @@
 import tensorflow as tf
-from collections import namedtuple
 from Models.train_vae import run_vae
+
 run_with_sample = True
-epochs = 50
-# model_type, n_required = 'IGR_I_Dis', 9
-model_type, n_required = 'IGR_SB_Dis', 49
-latent_discrete_n = n_required + 1
-run_closed_form_kl, num_of_discrete_param = True, 2
-model_type, n_required = 'GS_Dis', 10
-# latent_discrete_n = n_required
-# run_closed_form_kl, num_of_discrete_param = False, 1
-
-temps = [0.1]
-# temps = [0.1, 0.25, 0.5]
+num_of_repetitions = 1
+# temps = [0.1]
+temps = [0.1, 0.25, 0.5]
 # temps = [0.67, 0.85, 1.0]
+model_cases = {
+    1: {'model_type': 'IGR_I_Dis', 'n_required': 9},
+    # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9},
+    # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
+    #     'prior_file': './Results/mu_xi_unif_10_IGR_SB_Finite.pkl'},
+    # 4: {'model_type': 'IGR_SB_Dis', 'n_required': 49,
+    #     'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
+    #     'threshold': 0.9, 'truncation_option': 'quantile'},
+    # 5: {'model_type': 'GS_Dis', 'n_required': 10},
+}
+dataset_cases = {
+    1: {'dataset_name': 'mnist', 'architecture': 'dense'},
+    2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
+    # 3: {'dataset_name': 'celeb_a', 'architecture': 'conv_jointvae'},
+}
+for _, mod in model_cases.items():
+    hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
+             'num_of_discrete_var': 30,
+             'run_jv': False, 'γ': tf.constant(30.),
+             'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
+    i = 0
+    experiment = {}
+    for k, v in mod.items():
+        hyper[k] = v
 
-hyper = {'dataset_name': 'mnist', 'sample_size': 1, 'n_required': n_required, 'num_of_discrete_var': 30,
-         'latent_norm_n': 0, 'num_of_norm_var': 0, 'num_of_norm_param': 0,
-         'learning_rate': 0.001, 'batch_n': 64, 'epochs': epochs, 'architecture': 'dense',
-         'run_jv': False, 'γ': tf.constant(30.),
-         'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
-hyper.update({'latent_discrete_n': latent_discrete_n})
-hyper.update({'model_type': model_type, 'temp': 0.10,
-              # 'prior_file': './Results/mu_xi_unif_10_IGR_SB_Finite.pkl',
-              'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
-              'num_of_discrete_param': num_of_discrete_param,
-              'run_closed_form_kl': run_closed_form_kl})
+    hyper['latent_disc_n'] = hyper['n_required']
+    if hyper['model_type'].find('GS') >= 0:
+        hyper['run_closed_form_kl'] = False
+        hyper['num_of_discrete_param'] = 2
+    else:
+        hyper['latent_disc_n'] += 1
+        hyper['run_closed_form_kl'] = True
+        hyper['num_of_discrete_param'] = 1
+    for _, c in dataset_cases.items():
+        for t in temps:
+            i += 1
+            experiment.update({i: {}})
+            c.update({'temp': t})
+            for key, val in c.items():
+                experiment[i][key] = val
 
-Case = namedtuple('Case', 'dataset arch')
-datasets = [
-    Case(dataset='mnist', arch='dense'),
-    Case(dataset='fmnist', arch='dense'),
-    Case(dataset='celeb_a', arch='conv_jointvae'),
-]
-i = 0
-experiment = {}
-for d in datasets:
-    for t in temps:
-        i += 1
-        exp = {'dataset_name': d.dataset, 'architecture': d.arch, 'temp': t}
-        print(exp)
-        experiment.update({i: exp})
-
-for _, d in experiment.items():
-    for key, value in d.items():
-        hyper[key] = value
-    run_vae(hyper=hyper, run_with_sample=run_with_sample)
+    for _, d in experiment.items():
+        for key, value in d.items():
+            hyper[key] = value
+        for rep in range(num_of_repetitions):
+            print('\n')
+            print(hyper)
+            run_vae(hyper=hyper, run_with_sample=run_with_sample)
