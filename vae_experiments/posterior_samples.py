10f8719aae485833b3b21fda832a0452fb08c702
AndPotap <apotapczynski@gmail.com>
2020-02-05 11:01:04 -0500

    Changed labesl

--- vae_experiments/posterior_samples.py
+++ vae_experiments/posterior_samples.py
@@ -4,19 +4,19 @@ from Utils.posterior_sample_funcs import sample_from_posterior
 import seaborn as sns
 import pandas as pd
 
-dataset_name = 'celeb_a'
-run_with_sample = True
+dataset_name = 'mnist'
+run_with_sample = False
 plots_path = './Results/Outputs/'
 path = './Results/Current_Model/' + dataset_name + '/'
 models = {
-    # 1: {'model': 'igr_planar_cv', 'label': 'IGR_Planar(0.85)', 'type': 'IGR_Planar_Dis'},
-    # 3: {'model': 'igr_sb_cv', 'label': 'IGR_SB(0.1)', 'type': 'IGR_SB_Dis'},
-    # 5: {'model': 'igr_i_cv', 'label': 'IGR_I(1.0)', 'type': 'IGR_I_Dis'},
-    # 7: {'model': 'gs_cv', 'label': 'GS(1.0)', 'type': 'GS_Dis'},
-    2: {'model': 'igr_planar_20', 'label': 'IGR_Planar(0.1)', 'type': 'IGR_Planar_Dis'},
-    4: {'model': 'igr_sb_20', 'label': 'IGR_SB(0.03)', 'type': 'IGR_SB_Dis'},
-    6: {'model': 'igr_i_20', 'label': 'IGR_I(0.1)', 'type': 'IGR_I_Dis'},
-    8: {'model': 'gs_20', 'label': 'GS(0.25)', 'type': 'GS_Dis'},
+    1: {'model': 'igr_planar_cv', 'label': 'IGR-Planar(0.85)', 'type': 'IGR_Planar_Dis'},
+    3: {'model': 'igr_sb_cv', 'label': 'IGR-SB(0.15)', 'type': 'IGR_SB_Finite_Dis'},
+    5: {'model': 'igr_i_cv', 'label': 'IGR-I(1.0)', 'type': 'IGR_I_Dis'},
+    7: {'model': 'gs_cv', 'label': 'GS(1.0)', 'type': 'GS_Dis'},
+    # 2: {'model': 'igr_planar_20', 'label': 'IGR-Planar(0.3)', 'type': 'IGR_Planar_Dis'},
+    # 4: {'model': 'igr_sb_20', 'label': 'IGR-SB(0.02)', 'type': 'IGR_SB_Finite_Dis'},
+    # 6: {'model': 'igr_i_20', 'label': 'IGR-I(0.1)', 'type': 'IGR_I_Dis'},
+    # 8: {'model': 'gs_20', 'label': 'GS(0.25)', 'type': 'GS_Dis'},
 }
 for key, val in models.items():
     dirs = path + val['model'] + '/'
