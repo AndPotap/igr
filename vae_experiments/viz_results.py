c9afa1bbcca727aa9107fff13aff44baeddd9cbd
AndPotap <apotapczynski@gmail.com>
2020-02-05 11:16:52 -0500

    Shifted appeareance order

--- vae_experiments/viz_results.py
+++ vae_experiments/viz_results.py
@@ -16,13 +16,13 @@ from Utils.general import make_np_of_var_from_log_files, add_mean_std_plot_line
 #                                    path_to_files=path_to_files)
 path = './Results/elbo/mnist/'
 models = {
-    1: {'model': 'igr_planar_cv', 'label': 'IGR_Planar(0.85)', 'color': '#e41a1c'},
-    2: {'model': 'igr_planar_20', 'label': 'IGR_Planar(0.1)', 'color': '#e41a1c'},
-    3: {'model': 'igr_sb_cv', 'label': 'IGR_SB(0.1)', 'color': '#377eb8'},
-    4: {'model': 'igr_sb_20', 'label': 'IGR_SB(0.03)', 'color': '#377eb8'},
-    5: {'model': 'igr_i_cv', 'label': 'IGR_I(1.0)', 'color': '#4daf4a'},
-    6: {'model': 'igr_i_20', 'label': 'IGR_I(0.1)', 'color': '#4daf4a'},
+    1: {'model': 'igr_planar_cv', 'label': 'IGR-Planar(0.85)', 'color': '#e41a1c'},
+    3: {'model': 'igr_sb_cv', 'label': 'IGR-SB(0.15)', 'color': '#377eb8'},
+    5: {'model': 'igr_i_cv', 'label': 'IGR-I(1.0)', 'color': '#4daf4a'},
     7: {'model': 'gs_cv', 'label': 'GS(1.0)', 'color': '#984ea3'},
+    2: {'model': 'igr_planar_20', 'label': 'IGR-Planar(0.3)', 'color': '#e41a1c'},
+    4: {'model': 'igr_sb_20', 'label': 'IGR-SB(0.02)', 'color': '#377eb8'},
+    6: {'model': 'igr_i_20', 'label': 'IGR-I(0.1)', 'color': '#4daf4a'},
     8: {'model': 'gs_20', 'label': 'GS(0.25)', 'color': '#984ea3'},
 }
 for key, val in models.items():
