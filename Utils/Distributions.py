24aca05dfd3f52150a249b19e80dcb70b97d3170
AndPotap <apotapczynski@gmail.com>
2020-02-05 12:35:01 -0500

    Made exeption for GS

--- Utils/Distributions.py
+++ Utils/Distributions.py
@@ -189,7 +189,10 @@ def compute_loss(params: List[tf.Tensor], temp: tf.Tensor, probs: tf.Tensor, dis
     chosen_dist.generate_sample()
     psi_mean = tf.reduce_mean(chosen_dist.psi, axis=[0, 2, 3])
     if run_kl:
-        loss = psi_mean * (tf.math.log(psi_mean) - tf.math.log(probs[:chosen_dist.n_required + 1] + 1.e-20))
+        if dist_type == 'GS':
+            loss = psi_mean * (tf.math.log(psi_mean) - tf.math.log(probs[:chosen_dist.n_required] + 1.e-20))
+        else:
+            loss = psi_mean * (tf.math.log(psi_mean) - tf.math.log(probs[:chosen_dist.n_required + 1] + 1.e-20))
         loss = tf.reduce_sum(loss)
     else:
         loss = tf.reduce_sum((psi_mean - probs[:chosen_dist.n_required + 1]) ** 2)
