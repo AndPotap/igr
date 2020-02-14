b97da8a89a66bf2c9516d593ebae67ced65c207b
AndPotap <apotapczynski@gmail.com>
2020-02-14 16:35:01 -0500

    Took out unsued plotly files which eliminates the dependecy

--- Utils/viz_vae.py
+++ Utils/viz_vae.py
@@ -1,8 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import gridspec
-import plotly.graph_objs as go
-import plotly.offline as py
 import tensorflow as tf
 from scipy.stats import norm
 
@@ -50,7 +48,6 @@ def plot_originals(test_images, results_path):
 
 
 def plot_based_on_color_or_black(recon_image, rgb_location=2):
-    recon_image.shape
     if recon_image.shape[rgb_location] > 1:
         plt.imshow(recon_image)
     else:
@@ -75,25 +72,6 @@ def plot_grid_of_fixed_cont_traversal_along_all_disc_dim(model, fixed_cont_dim, 
             pointer += 1
             plt.axis('off')
     plt.savefig(plots_path + f'fixed_cont_{fixed_cont_dim:d}.png')
-    plt.close()
-
-
-def plot_grid_of_continuous_traversals_with_fixed_one_hot(model, fixed_cat, discrete_dim_n,
-                                                          cont_dim_n, traversal_n, plots_path):
-    all_latent_samples = traverse_all_cont_dim_with_fixed_discrete_dim(
-        fixed_cat=fixed_cat, cont_dim_n=cont_dim_n, discrete_dim_n=discrete_dim_n,
-        traversal_n=traversal_n)
-    images = get_images_from_samples(model, samples=all_latent_samples)
-    pointer = 0
-    plt.figure(figsize=(5, 4))
-    gs = set_grid_specifications(nrows=cont_dim_n, ncols=traversal_n)
-    for i in range(cont_dim_n):
-        for j in range(traversal_n):
-            plt.subplot(gs[i, j])
-            plot_based_on_color_or_black(recon_image=images[pointer, :, :, :])
-            pointer += 1
-            plt.axis('off')
-    plt.savefig(plots_path + f'grid_for_{fixed_cat:d}.png')
     plt.close()
 
 
@@ -145,19 +123,6 @@ def traverse_all_discrete_dim_with_fixed_cont_dim(fixed_cont_dim, cont_dim_n, di
     return all_latent_samples
 
 
-def traverse_all_cont_dim_with_fixed_discrete_dim(fixed_cat, cont_dim_n, discrete_dim_n, traversal_n):
-    all_latent_samples = []
-    for dim_to_traverse in range(cont_dim_n):
-        one_hot = get_one_hot_vector(category_represented=fixed_cat, vector_length=discrete_dim_n)
-        traversed_vec = get_traversed_vec(dim_to_traverse=dim_to_traverse, dim_n=cont_dim_n,
-                                          traversal_n=traversal_n)
-        for idx in range(traversal_n):
-            latent_sample = tf.concat([traversed_vec[idx], one_hot], axis=0)
-            latent_sample = tf.reshape(latent_sample, shape=(1, cont_dim_n + discrete_dim_n))
-            all_latent_samples.append(latent_sample)
-    return all_latent_samples
-
-
 def get_traversed_vec(dim_to_traverse, dim_n, traversal_n):
     list_of_traversed_vectors = []
     normal_traversal_values = get_normal_distribution_traversal_values(size=traversal_n)
@@ -193,40 +158,3 @@ def get_one_hot_vector(category_represented, vector_length):
     one_hot_vector = np.zeros(shape=vector_length)
     one_hot_vector[category_represented] = 1.
     return one_hot_vector
-
-
-def plot_grid_of_generated_digits(model, n_required: int, fig_size=10, digit_size=28, std_dev=2.,
-                                  filename='./Results/vae.html'):
-    figure = np.zeros((digit_size * fig_size, digit_size * fig_size))
-    grid_x = np.linspace(-std_dev, std_dev, fig_size)
-    grid_y = np.linspace(-std_dev, std_dev, fig_size)
-
-    for i, xi in enumerate(grid_x):
-        for j, yi in enumerate(grid_y):
-            if model.disc_latent_n > 0:
-                z_sample = np.random.normal(loc=0, scale=1, size=model.cont_latent_n)
-                c_sample = np.zeros(model.disc_latent_n * model.disc_var_num)
-                c_sample[j % n_required] = 1.
-                latent_sample = np.hstack((z_sample, c_sample))
-            else:
-                latent_sample = np.zeros(shape=model.cont_latent_n)
-                latent_sample[0], latent_sample[1] = xi, yi
-
-            digit = generate_digit_from_latent_sample(model, latent_sample, digit_size)
-            figure[i * digit_size: (i + 1) * digit_size, j * digit_size: (j + 1) * digit_size] = digit
-
-    trace = go.Heatmap(x=grid_x, y=grid_y, z=figure, colorscale='Viridis')
-    layout = go.Layout(yaxis=dict(autorange='reversed'))
-    fig = go.Figure(data=[trace], layout=layout)
-    py.plot(fig, filename=filename, auto_open=False)
-
-
-def generate_digit_from_latent_sample(model, latent_sample, digit_size):
-    latent_sample = np.broadcast_to(latent_sample, shape=(1, latent_sample.shape[0]))
-    generated = model.decode(latent_sample)[0].numpy()
-    # generated = model.decode(latent_sample).numpy()
-    digit = generated[0, :, :, 0].reshape(digit_size, digit_size)
-    digit = np.exp(digit) / (1 + np.exp(digit))
-    # digit[digit >= 0.5] = 1
-    # digit[digit < 0.5] = 0
-    return digit
