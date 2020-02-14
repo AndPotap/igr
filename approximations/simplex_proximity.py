52711e5d5b1b6f3e3c32754b7d53e6f1505db735
AndPotap <apotapczynski@gmail.com>
2020-02-14 17:22:33 -0500

    Changed to temp all

--- approximations/simplex_proximity.py
+++ approximations/simplex_proximity.py
@@ -19,11 +19,11 @@ models = {'IGR_I': [mu, xi], 'GS': [pi]}
 
 stats2run = ['median', 'p10', 'p20', 'std']
 
-results = run_simulation(samples=S, τ_grid=all_temps, models=models, threshold=threshold, stats2run=stats2run)
+results = run_simulation(samples=S, temp_grid=all_temps, models=models, threshold=threshold, stats2run=stats2run)
 
-plot_boxplots(model='GS', results=results, τ_grid=all_temps)
-plot_boxplots(model='IGR_I', results=results, τ_grid=all_temps)
-plot_stat_result(stat='median', models=models, results=results, τ_grid=all_temps)
+plot_boxplots(model='GS', results=results, temp_grid=all_temps)
+plot_boxplots(model='IGR_I', results=results, temp_grid=all_temps)
+plot_stat_result(stat='median', models=models, results=results, temp_grid=all_temps)
 
-igr_curve = fit_curve_to_result_along_grid(result=results['IGR_I']['median'], τ_grid=all_temps)
-gs_curve = fit_curve_to_result_along_grid(result=results['GS']['median'], τ_grid=all_temps)
+igr_curve = fit_curve_to_result_along_grid(result=results['IGR_I']['median'], temp_grid=all_temps)
+gs_curve = fit_curve_to_result_along_grid(result=results['GS']['median'], temp_grid=all_temps)
