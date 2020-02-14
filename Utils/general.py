7bddb7412cd6f7b91d247232684c248c833e5c24
AndPotap <apotapczynski@gmail.com>
2020-02-14 17:29:01 -0500

    Appended posterior sampling funcs

--- Utils/general.py
+++ Utils/general.py
@@ -1,7 +1,6 @@
 import datetime
 import logging
 import time
-import pickle
 from matplotlib import pyplot as plt
 import numba
 import seaborn as sns
@@ -10,6 +9,10 @@ from sklearn.kernel_ridge import KernelRidge
 import numpy as np
 import tensorflow as tf
 from Utils.Distributions import generate_sample
+import pickle
+from Utils.Distributions import IGR_I, GS, IGR_SB_Finite, IGR_Planar
+from Models.train_vae import construct_nets_and_optimizer
+from Utils.load_data import load_vae_dataset
 
 
 def plot_loss_and_initial_final_histograms(ax, loss_iter, p_samples, q_samples, q_samples_init,
@@ -206,6 +209,7 @@ def get_ending_with_timestamp(termination: str = '.pkl') -> str:
     return ending
 
 
+#  Simplex Proximity Functions
 def load_parameter_values(prior_file):
     with open(file=prior_file, mode='rb') as f:
         parameters = pickle.load(f)
@@ -309,3 +313,54 @@ def create_placeholders_for_statistical_results(stats, models, samples_per_stat)
         placeholders[model].update({stat: np.zeros(shape=samples_per_stat) for stat in stats})
 
     return placeholders
+
+
+# Posterior Sampling Funcs
+def sample_from_posterior(path_to_results, hyper_file, dataset_name, weights_file,
+                          model_type, run_with_sample=True):
+    with open(file=path_to_results + hyper_file, mode='rb') as f:
+        hyper = pickle.load(f)
+
+    data = load_vae_dataset(dataset_name=dataset_name, batch_n=hyper['batch_n'], epochs=hyper['epochs'],
+                            run_with_sample=run_with_sample, architecture=hyper['architecture'], hyper=hyper)
+    train_dataset, test_dataset, np_test_images, hyper = data
+
+    vae_opt = construct_nets_and_optimizer(hyper=hyper, model_type=model_type)
+    vae_opt.nets.load_weights(filepath=path_to_results + weights_file)
+
+    samples_n, total_test_images, im_idx = 100, 10_000, 0
+    # samples_n, total_test_images, im_idx = 100, 19_962, 0
+    shape = (total_test_images, samples_n, hyper['num_of_discrete_var'])
+    diff = np.zeros(shape=shape)
+
+    for test_image in test_dataset:
+        z, x_logit, params = vae_opt.perform_fwd_pass(test_image)
+        if model_type.find('Planar') > 0:
+            dist = determine_distribution(model_type=model_type, params=params, temp=hyper['temp'],
+                                          samples_n=samples_n, planar_flow=vae_opt.nets.planar_flow)
+        else:
+            dist = determine_distribution(model_type=model_type, params=params, temp=hyper['temp'],
+                                          samples_n=samples_n)
+        dist.generate_sample()
+        psi = dist.psi.numpy()
+        for i in range(psi.shape[0]):
+            for k in range(psi.shape[3]):
+                diff[im_idx, :, k] = calculate_distance_to_simplex(
+                    psi=psi[i, :, :, k], argmax_locs=np.argmax(psi[i, :, :, k], axis=0))
+            im_idx += 1
+    return diff
+
+
+def determine_distribution(model_type, params, temp, samples_n, planar_flow=None):
+    if model_type == 'IGR_I_Dis':
+        dist = IGR_I(mu=params[0], xi=params[1], sample_size=samples_n, temp=temp)
+    elif model_type == 'IGR_Planar_Dis':
+        dist = IGR_Planar(mu=params[0], xi=params[1], sample_size=samples_n, temp=temp,
+                          planar_flow=planar_flow)
+    elif model_type == 'GS_Dis':
+        dist = GS(log_pi=params[0], sample_size=samples_n, temp=temp)
+    elif model_type == 'IGR_SB_Finite_Dis':
+        dist = IGR_SB_Finite(mu=params[0], xi=params[1], sample_size=samples_n, temp=temp)
+    else:
+        raise RuntimeError
+    return dist
