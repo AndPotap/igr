5a5ae091262bc809fe446b023714576160a10f5d
AndPotap <apotapczynski@gmail.com>
2020-02-14 17:37:48 -0500

    Calls the general module now

--- Utils/MinimizeEmpiricalLoss.py
+++ Utils/MinimizeEmpiricalLoss.py
@@ -2,8 +2,7 @@ import time
 import numpy as np
 import tensorflow as tf
 from Utils.Distributions import compute_gradients, apply_gradients
-from Utils.initializations import initialize_mu_and_xi_for_logistic, initialize_mu_and_xi_equally
-from Utils.general import setup_logger
+from Utils.general import initialize_mu_and_xi_for_logistic, initialize_mu_and_xi_equally, setup_logger
 logger = setup_logger(log_file_name='./Log/discrete.log')
 
 
