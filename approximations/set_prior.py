427600e35ba6ca06e282940099ad91e4373189ab
AndPotap <apotapczynski@gmail.com>
2020-04-12 19:55:49 -0400

    Tested planar flow optimization problem

--- approximations/set_prior.py
+++ approximations/set_prior.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from Utils.MinimizeEmpiricalLoss import MinimizeEmpiricalLoss, get_initial_params_for_model_type
 from Utils.general import get_for_approx, plot_loss_and_initial_final_histograms, sample_from_approx
+from Models.VAENet import create_nested_planar_flow
 
 sample_size = int(1.e3)
 total_iterations = 3 * int(1.e1)
@@ -32,12 +33,18 @@ save_parameters = False
 probs, p_samples, results_file = get_for_approx(run_against, categories_n, samples_plot_n)
 
 # Train model
-# ===========================================================================================================
+# =========================================================================================================
 params, params_init = get_initial_params_for_model_type(model_type=model_type, shape=shape)
-
+if model_type.find('Planar'):
+    nested_layers = 2
+    var_num = 1
+    planar_flow = create_nested_planar_flow(nested_layers, categories_n - 1, var_num, initializer='zeros')
+else:
+    planar_flow = None
 minimizer = MinimizeEmpiricalLoss(learning_rate=learning_rate, temp=temp, sample_size=sample_size,
-                                  run_kl=False, params=params,
-                                  max_iterations=total_iterations, model_type=model_type, threshold=threshold)
+                                  run_kl=False, params=params, max_iterations=total_iterations,
+                                  model_type=model_type, threshold=threshold,
+                                  planar_flow=planar_flow)
 minimizer.run_iteratively = True
 minimizer.optimize_model(probs=probs)
 
@@ -47,12 +54,13 @@ if save_parameters:
             pickle.dump(obj={'mu': params[0].numpy(), 'xi': params[1].numpy()}, file=f)
 
 # Run samples
-# ===========================================================================================================
+# ==========================================================================================================
 q_samples, q_samples_init = sample_from_approx(params, params_init, temp, model_type, p_samples,
-                                               samples_plot_n, minimizer.threshold)
+                                               samples_plot_n, minimizer.threshold,
+                                               minimizer.planar_flow)
 
 # Plot Loss and Histograms
-# ===========================================================================================================
+# ==========================================================================================================
 plt.style.use(style='ggplot')
 fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(8, 6))
 
