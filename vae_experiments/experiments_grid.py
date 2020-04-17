d435e0c224047bf22751a16f39e22bfd33339eff
AndPotap <apotapczynski@gmail.com>
2020-04-17 19:58:02 -0400

    Reverted batch_size

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -23,7 +23,7 @@ dataset_cases = {
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'num_of_discrete_var': 30, 'test_with_one_hot': True,
          'sample_from_cont_kl': True, 'sample_from_disc_kl': True,
-         'learning_rate': 0.001, 'batch_n': 100, 'epochs': 100,
+         'learning_rate': 0.001, 'batch_n': 64, 'epochs': 100,
          'sample_size': 1, 'sample_size_testing': int(1.e3),
          'run_jv': False, 'gamma': tf.constant(30.), 'check_every': 10,
          'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
