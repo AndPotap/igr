136b1ef31b5ae1519bddeef33e3c7f95c0cf3eba
AndPotap <apotapczynski@gmail.com>
2020-04-22 16:30:24 -0400

    Tested softmax++ integral

--- Tests/TestInter.py
+++ Tests/TestInter.py
@@ -1,12 +1,37 @@
 import numpy as np
 from Utils.interpretation_funcs import compute_cat_prob
+from Utils.interpretation_funcs import compute_softmax_pp_prob
+
+
+def test_softmax_pp_inter():
+    tolerance = 1.e-1
+    samples_n = int(1.e6)
+    mu = np.array([3., 2., 1.])
+    sigma = np.array([4., 5., 1.])
+    cov = np.diag(sigma ** 2)
+    categories_n = mu.shape[0]
+    z = np.random.multivariate_normal(mu, cov, size=samples_n)
+
+    w_argmax = np.argmax(z, axis=1)
+    ans = np.zeros(categories_n + 1)
+    for k in range(categories_n):
+        ind_k = w_argmax == k
+        total_positive = ind_k.shape[0]
+        count_positive = np.sum(z[ind_k, k] > 0)
+        ans[k] = count_positive / total_positive
+    ans[categories_n] = 1 - np.sum(ans[:categories_n])
+
+    approx = compute_softmax_pp_prob(mu, sigma)
+
+    diff = np.linalg.norm(approx - ans) / np.linalg.norm(ans)
+    print(f'\nDiff {diff:1.2e}')
+    assert diff < tolerance
 
 
 def test_gaussian_inter():
     tolerance = 1.e-1
     mu = np.array([3., 2., 1., 0.1])
     sigma = np.array([0.1, 1., 1., 3])
-    # sigma = np.array([1., 1., 1., 1.])
     cov = np.diag(sigma ** 2)
     samples_n = int(1.e4)
     categories_n = mu.shape[0]
