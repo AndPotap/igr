de70c26ee85dc024b363bdf75135aeb327737643
AndPotap <apotapczynski@gmail.com>
2020-05-06 16:31:14 -0400

    Set new hyperparameters

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -3,17 +3,17 @@ from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = True
 num_of_repetitions = 1
-temps = [0.10]
+temps = [0.50]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 model_cases = {
-    1: {'model_type': 'IGR_I_Dis', 'n_required': 9},
+    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_SB_Finite.pkl'},
     # 4: {'model_type': 'IGR_SB_Dis', 'n_required': 49,
     #     'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
     #     'threshold': 0.9, 'truncation_option': 'quantile'},
-    # 5: {'model_type': 'GS_Dis', 'n_required': 10},
+    5: {'model_type': 'GS_Dis', 'n_required': 10},
 }
 dataset_cases = {
     1: {'dataset_name': 'mnist', 'architecture': 'dense'},
@@ -21,12 +21,12 @@ dataset_cases = {
     # 3: {'dataset_name': 'celeb_a', 'architecture': 'conv_jointvae'},
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
-         'num_of_discrete_var': 30,
-         'test_with_one_hot': False,
+         'num_of_discrete_var': 20,
+         'test_with_one_hot': True,
          'sample_from_cont_kl': True, 'sample_from_disc_kl': True,
-         'learning_rate': 0.001, 'batch_n': 64, 'epochs': 100,
-         'sample_size': 1, 'sample_size_testing': int(1.e2),
-         'stick_the_landing': True,
+         'learning_rate': 3 * 1.e-4, 'batch_n': 100, 'epochs': 100,
+         'sample_size': 1, 'sample_size_testing': int(1.e3),
+         'stick_the_landing': False,
          'check_every': 10,
          'run_jv': False, 'gamma': tf.constant(30.),
          'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
