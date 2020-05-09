ef92b5e6b8e69f63bf33e30cb5d2b246e821260a
AndPotap <apotapczynski@gmail.com>
2020-05-08 20:10:05 -0400

    Changed the latests parameters

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -3,17 +3,18 @@ from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = True
 num_of_repetitions = 1
-temps = [0.50]
+temps = [0.15]  # delta IGR set for this
+# temps = [0.15]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 model_cases = {
-    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9},
+    1: {'model_type': 'IGR_I_Dis', 'n_required': 9},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_SB_Finite.pkl'},
     # 4: {'model_type': 'IGR_SB_Dis', 'n_required': 49,
     #     'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
     #     'threshold': 0.9, 'truncation_option': 'quantile'},
-    5: {'model_type': 'GS_Dis', 'n_required': 10},
+    # 5: {'model_type': 'GS_Dis', 'n_required': 10},
 }
 dataset_cases = {
     1: {'dataset_name': 'mnist', 'architecture': 'dense'},
@@ -22,9 +23,9 @@ dataset_cases = {
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'test_with_one_hot': True,
-         'sample_from_disc_kl': False,
+         'sample_from_disc_kl': True,
          'check_every': 1,
-         'sample_size_testing': int(1.e0),
+         'sample_size_testing': int(1.e2),
          'learning_rate': 3 * 1.e-4,
          'batch_n': 100, 'epochs': 100,
          'sample_from_cont_kl': True, 'num_of_discrete_var': 20,
