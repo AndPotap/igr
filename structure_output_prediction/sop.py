d58feab1ad52be2bf6df34c805c5aedf8a30baa7
AndPotap <apotapczynski@gmail.com>
2020-05-11 19:39:04 -0400

    Added iso variant to script

--- structure_output_prediction/sop.py
+++ structure_output_prediction/sop.py
@@ -2,16 +2,22 @@ from Utils.load_data import load_mnist_sop_data
 from Models.SOPOptimizer import run_sop
 import tensorflow as tf
 
-model_type = 'GS'
+# model_type = 'GS'
+# model_type = 'IGR_I'
+model_type = 'IGR_Iso'
 hyper = {'width_height': (14, 28, 1),
          'units_per_layer': 240,
          'model_type': model_type,
          'batch_size': 64,
-         # 'learning_rate': 0.003,
          'learning_rate': 0.01,
          'weight_decay': 1.e-3,
+         # 'learning_rate': 0.001,
+         # 'weight_decay': 1 * 1.e-4,
+         # 'learning_rate': 0.005,
+         # 'weight_decay': 0 * 1.e-4,
          'epochs': 1 * int(1.e2),
          'iter_per_epoch': 937,
-         'temp': tf.constant(0.67)}
+         'test_sample_size': 100,
+         'temp': tf.constant(1.0)}
 data = load_mnist_sop_data(batch_n=hyper['batch_size'])
 run_sop(hyper=hyper, results_path='./Log/', data=data)
