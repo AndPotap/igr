800bba6ee92ea51a61e0ffd2fe744ddbb41dc667
AndPotap <apotapczynski@gmail.com>
2020-05-12 11:07:38 -0400

    Added best performing hyper to dict

--- structure_output_prediction/sop.py
+++ structure_output_prediction/sop.py
@@ -3,17 +3,15 @@ from Models.SOPOptimizer import run_sop
 import tensorflow as tf
 
 # model_type = 'GS'
-# model_type = 'IGR_I'
-# model_type = 'IGR_Iso'
-model_type = 'IGR_Planar'
+model_type = 'IGR_I'
+# model_type = 'IGR_Planar'
 hyper = {'width_height': (14, 28, 1),
          'units_per_layer': 240,
          'model_type': model_type,
-         'batch_size': 64,
+         'batch_size': 100,
          'learning_rate': 0.01,
          'weight_decay': 1.e-3,
-         # 'learning_rate': 0.005,
-         # 'weight_decay': 1 * 1.e-4,
+         'min_learning_rate': 5.e-4,
          'epochs': 1 * int(1.e2),
          'iter_per_epoch': 937,
          'test_sample_size': 100,
