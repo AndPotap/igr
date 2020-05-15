b5d2a050e919c54fde4f487d0e9abcfa57cf319b
AndPotap <apotapczynski@gmail.com>
2020-05-14 20:09:28 -0400

    Added params to run IGR

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -3,11 +3,11 @@ from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = True
 num_of_repetitions = 1
-temps = [0.67]
+temps = [0.15]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 model_cases = {
-    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
-    #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
+    1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
+        'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 'prior_file': './Results/mu_xi_unif_' + str(round(temps[0] * 100)) + '_IGR_I.pkl'},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
@@ -15,7 +15,7 @@ model_cases = {
     # 4: {'model_type': 'IGR_SB_Dis', 'n_required': 49,
     #     'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
     #     'threshold': 0.9, 'truncation_option': 'quantile'},
-    5: {'model_type': 'GS_Dis', 'n_required': 10},
+    # 5: {'model_type': 'GS_Dis', 'n_required': 10},
 }
 dataset_cases = {
     1: {'dataset_name': 'mnist', 'architecture': 'dense'},
@@ -23,10 +23,10 @@ dataset_cases = {
     # 3: {'dataset_name': 'celeb_a', 'architecture': 'conv_jointvae'},
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
-         'test_with_one_hot': True,
-         'sample_from_disc_kl': False,
-         'check_every': 10,
          'sample_size_testing': 1 * int(1.e2),
+         'check_every': 10,
+         'sample_from_disc_kl': False,
+         'test_with_one_hot': True,
          'learning_rate': 3 * 1.e-4,
          'batch_n': 100, 'epochs': 100,
          'sample_from_cont_kl': True, 'num_of_discrete_var': 20,
