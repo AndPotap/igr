2f65d37176cb48cebdd82449b6c230db0f8f2547
AndPotap <apotapczynski@gmail.com>
2020-05-19 13:12:24 -0400

    Added old hyperparameters to experiments script

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -3,7 +3,7 @@ from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
 num_of_repetitions = 1
-temps = [0.15]
+temps = [0.30]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 model_cases = {
     # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
@@ -18,7 +18,8 @@ model_cases = {
     # 5: {'model_type': 'GS_Dis', 'n_required': 10},
 }
 dataset_cases = {
-    1: {'dataset_name': 'mnist', 'architecture': 'dense'},
+    # 1: {'dataset_name': 'mnist', 'architecture': 'dense'},
+    1: {'dataset_name': 'mnist', 'architecture': 'dense_nonlinear'},
     # 2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
     # 3: {'dataset_name': 'celeb_a', 'architecture': 'conv_jointvae'},
 }
@@ -27,9 +28,11 @@ hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'check_every': 25,
          'sample_from_disc_kl': True,
          'test_with_one_hot': True,
-         'learning_rate': 3 * 1.e-4,
-         'batch_n': 100, 'epochs': 200,
-         'sample_from_cont_kl': True, 'num_of_discrete_var': 20,
+         # 'learning_rate': 3 * 1.e-4,
+         'learning_rate': 1 * 1.e-3,
+         'batch_n': 64, 'epochs': 200,
+         'num_of_discrete_var': 30,
+         'sample_from_cont_kl': True,
          'sample_size': 1, 'stick_the_landing': True,
          'run_jv': False, 'gamma': tf.constant(30.),
          'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
