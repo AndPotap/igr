d429553d42c8716806c34b4b613d43e87b38e366
AndPotap <apotapczynski@gmail.com>
2020-05-19 18:42:45 -0400

    Left latest sop script

--- structure_output_prediction/sop.py
+++ structure_output_prediction/sop.py
@@ -2,14 +2,15 @@ from Utils.load_data import load_mnist_sop_data
 from Models.SOPOptimizer import run_sop
 import tensorflow as tf
 
-# model_type = 'GS'
+model_type = 'GS'
 # model_type = 'IGR_I'
-model_type = 'IGR_Planar'
+# model_type = 'IGR_Planar'
 hyper = {'width_height': (14, 28, 1),
          'units_per_layer': 240,
          'model_type': model_type,
          'batch_size': 100,
          'learning_rate': 0.01,
+         # 'learning_rate': 0.001,
          'weight_decay': 1.e-3,
          'min_learning_rate': 1.e-4,
          'epochs': 1 * int(1.e2),
