592c819a48e5338571dbc5bc111ef0ea0cf94f0b
AndPotap <apotapczynski@gmail.com>
2020-05-19 17:56:43 -0400

    Added nonlinear option in hyper

--- structure_output_prediction/sop.py
+++ structure_output_prediction/sop.py
@@ -2,9 +2,9 @@ from Utils.load_data import load_mnist_sop_data
 from Models.SOPOptimizer import run_sop
 import tensorflow as tf
 
-model_type = 'GS'
+# model_type = 'GS'
 # model_type = 'IGR_I'
-# model_type = 'IGR_Planar'
+model_type = 'IGR_Planar'
 hyper = {'width_height': (14, 28, 1),
          'units_per_layer': 240,
          'model_type': model_type,
@@ -17,7 +17,8 @@ hyper = {'width_height': (14, 28, 1),
          'iter_per_epoch': 937,
          'test_sample_size': 100,
          # 'architecture': 'double_linear',
-         'architecture': 'triple_linear',
+         # 'architecture': 'triple_linear',
+         'architecture': 'nonlinear',
          'sample_size': 1,
          'temp': tf.constant(1.0)}
 data = load_mnist_sop_data(batch_n=hyper['batch_size'])
