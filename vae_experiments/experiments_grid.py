40695dfa04841ae0bdc676b50fc0962708d7040d
AndPotap <apotapczynski@gmail.com>
2020-05-22 13:49:10 -0400

    Added the relax gs dis into the script hyper

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -3,11 +3,11 @@ from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
 num_of_repetitions = 1
-temps = [0.15]
+temps = [0.67]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 model_cases = {
-    1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
-        'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
+    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
+    #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
@@ -16,6 +16,7 @@ model_cases = {
     #     'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
     #     'threshold': 0.9, 'truncation_option': 'quantile'},
     # 5: {'model_type': 'GS_Dis', 'n_required': 10},
+    6: {'model_type': 'Relax_GS_Dis', 'n_required': 10},
 }
 dataset_cases = {
     1: {'dataset_name': 'mnist', 'architecture': 'dense'},
@@ -25,7 +26,7 @@ dataset_cases = {
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
-         'check_every': 25,
+         'check_every': 1,
          'sample_from_disc_kl': True,
          'test_with_one_hot': True,
          'epochs': 200,
