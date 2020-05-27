0d5dcb04097920bb6049f419e9d9829d7cf9db3f
AndPotap <apotapczynski@gmail.com>
2020-05-27 07:44:36 -0400

    Updated hyper for experiments grid

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -3,12 +3,12 @@ from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
 num_of_repetitions = 1
-# temps = [1.67]
-temps = [5.00]
+temps = [0.15]
+# temps = [5.00]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 model_cases = {
-    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
-    #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
+    1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
+        'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
@@ -18,24 +18,24 @@ model_cases = {
     #     'threshold': 0.9, 'truncation_option': 'quantile'},
     # 5: {'model_type': 'GS_Dis', 'n_required': 10},
     # 6: {'model_type': 'Relax_GS_Dis', 'n_required': 10},
-    7: {'model_type': 'Relax_Ber_Dis', 'n_required': 200},
+    # 7: {'model_type': 'Relax_Ber_Dis', 'n_required': 200},
 }
 dataset_cases = {
-    1: {'dataset_name': 'mnist', 'architecture': 'dense'},
+    # 1: {'dataset_name': 'mnist', 'architecture': 'dense'},
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense_nonlinear'},
-    # 2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
+    2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
     # 3: {'dataset_name': 'celeb_a', 'architecture': 'conv_jointvae'},
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
-         'check_every': 50,
+         'check_every': 1,
          'sample_from_disc_kl': True,
          'test_with_one_hot': True,
          'epochs': 1000,
          'learning_rate': 3 * 1.e-4,
          'batch_n': 100,
-         # 'num_of_discrete_var': 20,
-         'num_of_discrete_var': 1,  # for bernoulli experiment
+         'num_of_discrete_var': 20,
+         # 'num_of_discrete_var': 1,  # for bernoulli experiment
          # 'batch_n': 64,
          # 'num_of_discrete_var': 30,
          'sample_from_cont_kl': True,
