9e9451e4b1e48aba044ba9de45699e9c58b00c51
AndPotap <apotapczynski@gmail.com>
2020-05-27 20:57:08 -0400

    Added seeds to sop script

--- structure_output_prediction/sop.py
+++ structure_output_prediction/sop.py
@@ -1,4 +1,3 @@
-from Utils.load_data import load_mnist_sop_data
 from Models.SOPOptimizer import run_sop_for_all_cases
 import tensorflow as tf
 
@@ -9,7 +8,12 @@ model_type = 'GS'
 # num_of_repetitions = 1
 # architectures = ['double_linear']
 # sample_sizes = [1]
-num_of_repetitions = 3
+
+# import numpy as np
+# np.random.seed(21)
+# seeds = np.random.randint(low=1, high=int(1.e4), size=5)
+# seeds = [5328, 5945, 8965, 49, 9337]
+seeds = [5328]
 architectures = ['double_linear', 'triple_linear', 'nonlinear']
 sample_sizes = [1, 5, 50]
 baseline_hyper = {'width_height': (14, 28, 1),
@@ -33,5 +37,4 @@ for arch in architectures:
     for sample_size in sample_sizes:
         variant_hyper.update({idx: {'sample_size': sample_size, 'architecture': arch}})
         idx += 1
-data = load_mnist_sop_data(batch_n=baseline_hyper['batch_size'])
-run_sop_for_all_cases(baseline_hyper, variant_hyper, data, num_of_repetitions)
+run_sop_for_all_cases(baseline_hyper, variant_hyper, seeds)
