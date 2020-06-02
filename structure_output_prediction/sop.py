4ef67cb48405e95b635389bb9be4c9c090f442a5
AndPotap <apotapczynski@gmail.com>
2020-06-02 06:18:02 -0400

    Changed defaults on SOP optimizer

--- structure_output_prediction/sop.py
+++ structure_output_prediction/sop.py
@@ -5,13 +5,13 @@ model_type = 'IGR_I'
 # model_type = 'IGR_Planar'
 # model_type = 'IGR_SB'
 # model_type = 'GS'
-temp = 0.1
+temp = 1.0
 
 # import numpy as np
 # np.random.seed(21)
 # seeds = np.random.randint(low=1, high=int(1.e4), size=5)
-# seeds = [5328, 5945, 8965, 49, 9337]
-seeds = [5328]
+seeds = [5328, 5945, 8965, 49, 9337]
+# seeds = [5328]
 architectures = ['double_linear']
 sample_sizes = [1]
 # architectures = ['double_linear', 'triple_linear', 'nonlinear']
@@ -25,7 +25,7 @@ baseline_hyper = {'width_height': (14, 28, 1),
                   'min_learning_rate': 1.e-4,
                   'epochs': 1 * int(1.e2),
                   # 'check_every': 50,
-                  'check_every': 25,
+                  'check_every': 50,
                   'iter_per_epoch': 937,
                   'test_sample_size': 1 * int(1.e2),
                   'architecture': 'nonlinear',
