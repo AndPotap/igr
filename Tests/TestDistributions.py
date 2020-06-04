b6b4ddae0a441ab7a99bbd6780fd6ef29f615bbb
AndPotap <apotapczynski@gmail.com>
2020-06-03 20:18:28 -0400

    Integral passes uniform and general case

--- Tests/TestDistributions.py
+++ Tests/TestDistributions.py
@@ -11,7 +11,33 @@ from Utils.Distributions import compute_h_f, compute_igr_probs
 
 class TestDistributions(unittest.TestCase):
 
-    def test_proba_integral_in_uniform_case(self):
+    def test_probs_integral_general_case(self):
+        test_tolerance = 1.e-5
+        batch_size, categories_n, sample_size, num_of_vars = 3, 9, 1, 1
+
+        shape = (batch_size, categories_n, sample_size, num_of_vars)
+        mu = np.random.normal(loc=-1.0, size=shape)
+        mu_tf = tf.constant(mu, dtype=tf.float32)
+        sigma = np.exp(np.random.normal(size=shape))
+        sigma_tf = tf.constant(sigma, dtype=tf.float32)
+        approx = compute_igr_probs(mu_tf, sigma_tf).numpy()
+
+        ans = np.zeros((batch_size, categories_n + 1, sample_size, num_of_vars))
+        for b in range(batch_size):
+            for s in range(sample_size):
+                for var in range(num_of_vars):
+                    mu0, sigma0 = mu[b, :, s, var], sigma[b, :, s, var]
+                    for j in range(categories_n):
+                        ans[b, j, s, var], _ = quad(compute_probas_integrand2, a=0., b=np.inf,
+                                                    args=(mu0, sigma0, j))
+                    ans[b, categories_n, s, var] = 1. - np.sum(ans[b, :, s, var])
+
+        print(f'\nTEST: Gaussian Probas integral')
+        diff = np.linalg.norm(approx - ans) / np.linalg.norm(ans)
+        print(f'\nDiff {diff:1.3e}')
+        self.assertTrue(expr=diff < test_tolerance)
+
+    def test_probs_integral_in_uniform_case(self):
         test_tolerance = 1.e-5
         batch_size, categories_n, sample_size, num_of_vars = 3, 9, 1, 1
 
@@ -28,13 +54,11 @@ class TestDistributions(unittest.TestCase):
                 for var in range(num_of_vars):
                     mu0, sigma0 = mu[b, :, s, var], sigma[b, :, s, var]
                     for j in range(categories_n):
-                        # ans[b, j, s, var], _ = quad(compute_probas_integrand, a=0., b=np.inf,
-                        #                             args=(mu0, sigma0, j))
-                        ans[b, j, s, var], _ = quad(compute_probas_integrand2, a=0., b=np.inf,
+                        ans[b, j, s, var], _ = quad(compute_probas_integrand, a=0., b=np.inf,
                                                     args=(mu0, sigma0, j))
                     ans[b, categories_n, s, var] = 1. - np.sum(ans[b, :, s, var])
 
-        print(f'\nTEST: Gaussian Probas integral')
+        print(f'\nTEST: Gaussian Probas integral for uniform case')
         diff = np.linalg.norm(approx - ans) / np.linalg.norm(ans)
         print(f'\nDiff {diff:1.3e}')
         self.assertTrue(expr=diff < test_tolerance)
