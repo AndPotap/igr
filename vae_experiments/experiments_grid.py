6e34d5002c9a474a4d738de49e6452ce4dc9c454
AndPotap <apotapczynski@gmail.com>
2020-06-03 20:19:03 -0400

    Set new default for nonlinear experiments

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -2,19 +2,19 @@ import tensorflow as tf
 from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
-temps = [0.15]
+temps = [0.67]
 # temps = [5.00]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 
 # import numpy as np
 # np.random.seed(21)
 # seeds = np.random.randint(low=1, high=int(1.e4), size=5)
-seeds = [5328, 5945, 8965, 49, 9337]
-# seeds = [49]
+# seeds = [5328, 5945, 8965, 49, 9337]
+seeds = [49]
 
 model_cases = {
-    1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
-        'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
+    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
+    #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
@@ -22,27 +22,27 @@ model_cases = {
     # 4: {'model_type': 'IGR_SB_Dis', 'n_required': 49,
     #     'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
     #     'threshold': 0.9, 'truncation_option': 'quantile'},
-    # 5: {'model_type': 'GS_Dis', 'n_required': 10},
+    5: {'model_type': 'GS_Dis', 'n_required': 10},
     # 6: {'model_type': 'Relax_GS_Dis', 'n_required': 10},
     # 7: {'model_type': 'Relax_Ber_Dis', 'n_required': 200},
     # 8: {'model_type': 'Relax_IGR', 'n_required': 9},
 }
 dataset_cases = {
-    # 1: {'dataset_name': 'mnist', 'architecture': 'dense'},
+    1: {'dataset_name': 'mnist', 'architecture': 'dense'},
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense_nonlinear'},
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense_relax'},
-    2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
-    3: {'dataset_name': 'omniglot', 'architecture': 'dense'},
+    # 2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
+    # 3: {'dataset_name': 'omniglot', 'architecture': 'dense'},
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
          'check_every': 50,
-         'sample_from_disc_kl': False,
+         'sample_from_disc_kl': True,
          'test_with_one_hot': True,
-         'epochs': 100,
-         'learning_rate': 3 * 1.e-4,
+         'epochs': 300,
+         'learning_rate': 5 * 1.e-4,
          'batch_n': 100,
-         'num_of_discrete_var': 20,
+         'num_of_discrete_var': 40,
          # 'num_of_discrete_var': 1,  # for bernoulli experiment
          # 'batch_n': 64,
          # 'num_of_discrete_var': 30,
