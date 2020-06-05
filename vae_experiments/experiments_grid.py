fabfb1e9ebd95447b1f4d75370e16336c2955c11
AndPotap <apotapczynski@gmail.com>
2020-06-05 19:33:23 -0400

    Set prior params to run IGR with log_probs

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
-temps = [0.67]
+temps = [0.15]
 # temps = [5.00]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 
@@ -13,8 +13,8 @@ temps = [0.67]
 seeds = [9335]
 
 model_cases = {
-    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
-    #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
+    1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
+        'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
@@ -25,25 +25,25 @@ model_cases = {
     # 5: {'model_type': 'GS_Dis', 'n_required': 10},
     # 6: {'model_type': 'Relax_GS_Dis', 'n_required': 10},
     # 7: {'model_type': 'Relax_Ber_Dis', 'n_required': 200},
-    8: {'model_type': 'Relax_IGR', 'n_required': 9},
+    # 8: {'model_type': 'Relax_IGR', 'n_required': 9},
 }
 dataset_cases = {
-    # 1: {'dataset_name': 'mnist', 'architecture': 'dense'},
+    1: {'dataset_name': 'mnist', 'architecture': 'dense'},
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense_nonlinear'},
-    1: {'dataset_name': 'mnist', 'architecture': 'dense_relax'},
+    # 1: {'dataset_name': 'mnist', 'architecture': 'dense_relax'},
     # 2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
     # 3: {'dataset_name': 'omniglot', 'architecture': 'dense'},
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
-         'check_every': 10,
-         'sample_from_disc_kl': True,
+         'check_every': 25,
+         'sample_from_disc_kl': False,
          'test_with_one_hot': True,
          'epochs': 100,
          'learning_rate': 1 * 1.e-4,
          'batch_n': 100,
          'num_of_discrete_var': 20,
-         'stick_the_landing': True,
+         'stick_the_landing': False,
          'sample_size': 1, 'sample_from_cont_kl': True,
          'run_jv': False, 'gamma': tf.constant(30.),
          'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
