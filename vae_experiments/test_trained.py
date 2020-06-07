c2d6117aa7c771aeca88251daa63574484f201c6
AndPotap <apotapczynski@gmail.com>
2020-06-07 19:47:28 -0400

    Made script to explore NLL

--- vae_experiments/test_trained.py
+++ vae_experiments/test_trained.py
@@ -1,8 +1,12 @@
 import time
-import pickle
-import tensorflow as tf
+# from Utils.general import calculate_test_log_likelihood, setup_logger
+# from Utils.general import load_hyper_and_data
+# from Utils.general import setup_optimizer
+from Utils.estimate_loglike import calculate_test_log_likelihood
+from Utils.estimate_loglike import setup_logger
+from Utils.estimate_loglike import load_hyper_and_data
 from Models.train_vae import construct_nets_and_optimizer
-from Utils.load_data import load_vae_dataset
+# from Utils.estimate_loglike import setup_optimizer
 
 tic = time.time()
 dataset_name = 'mnist'
@@ -15,48 +19,20 @@ models = {
     3: {'model_dir': 'pf', 'model_type': 'IGR_Planar_Dis'},
     4: {'model_dir': 'sb', 'model_type': 'IGR_SB_Finite_Dis'},
 }
-select_case = 4
-run_with_sample = False
+select_case = 2
+run_with_sample = True
 samples_n = 1 * int(1.e3)
 
-hyper_file = 'hyper.pkl'
 weights_file = 'vae.h5'
 model_type = models[select_case]['model_type']
 path_to_trained_models += models[select_case]['model_dir'] + '/'
 
-with open(file=path_to_trained_models + hyper_file, mode='rb') as f:
-    hyper = pickle.load(f)
-
-batch_n = hyper['batch_n']
-# batch_n = int(1.e4)
-hyper['sample_size_testing'] = samples_n
-tf.random.set_seed(seed=hyper['seed'])
-data = load_vae_dataset(dataset_name=dataset_name, batch_n=batch_n, epochs=hyper['epochs'],
-                        run_with_sample=run_with_sample,
-                        architecture=hyper['architecture'], hyper=hyper)
-train_dataset, test_dataset, np_test_images, hyper = data
-epoch = hyper['epochs']
+test_dataset, hyper, epoch = load_hyper_and_data(path_to_trained_models, dataset_name,
+                                                 samples_n, run_with_sample)
+# vae_opt = setup_optimizer(path_to_trained_models, hyper, model_type)
 vae_opt = construct_nets_and_optimizer(hyper=hyper, model_type=model_type)
 vae_opt.nets.load_weights(filepath=path_to_trained_models + weights_file)
 vae_opt.test_with_one_hot = True
+logger = setup_logger(log_file_name='./Log/nll.txt', logger_name='nll')
 
-test_loss_mean = tf.keras.metrics.Mean()
-for x_test in test_dataset:
-    loss = vae_opt.compute_losses_from_x_wo_gradients(x_test,
-                                                      sample_from_cont_kl=False,
-                                                      sample_from_disc_kl=False)
-    test_loss_mean(loss)
-# train_loss_mean = tf.keras.metrics.Mean()
-# for x_train in train_dataset:
-#     loss, *_ = vae_opt.compute_losses_from_x_wo_gradients(x_train,
-#                                                           sample_from_cont_kl=False,
-#                                                           sample_from_disc_kl=False)
-#     train_loss_mean(loss)
-
-evaluation_print = f'Epoch {epoch:4d} || '
-evaluation_print += f'TeELBOC {-test_loss_mean.result():2.5e} || '
-# evaluation_print += f'Train Loss {train_loss_mean.result():2.5e} || '
-evaluation_print += f'{model_type} || '
-toc = time.time()
-evaluation_print += f'Time: {toc - tic:2.2e} sec'
-print(evaluation_print)
+calculate_test_log_likelihood(logger, vae_opt, test_dataset, epoch, model_type, tic)
