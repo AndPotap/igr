2b757c2ea3fc2ae6024a09471dfebcbd01f24fec
AndPotap <apotapczynski@gmail.com>
2020-06-08 19:29:33 -0400

    Logs hyper and weights

--- vae_experiments/test_trained.py
+++ vae_experiments/test_trained.py
@@ -3,16 +3,17 @@ from Utils.estimate_loglike import get_available_logs
 from Utils.estimate_loglike import manage_files
 from Utils.estimate_loglike import setup_logger
 
-run_with_sample = True
+run_with_sample = False
+check_only = False
 samples_n = 1 * int(1.e3)
 # datasets = ['mnist', 'fmnist']
-datasets = ['mnist']
+datasets = ['omniglot']
 architectures = ['nonlinear']
 models = {
-    # 1: {'model_dir': 'igr', 'model_type': 'IGR_I_Dis'},
-    2: {'model_dir': 'gs', 'model_type': 'GS_Dis'},
-    # 3: {'model_dir': 'pf', 'model_type': 'IGR_Planar_Dis'},
-    # 4: {'model_dir': 'sb', 'model_type': 'IGR_SB_Finite_Dis'},
+    1: {'model_dir': 'igr', 'model_type': 'IGR_I_Dis'},
+    2: {'model_dir': 'pf', 'model_type': 'IGR_Planar_Dis'},
+    3: {'model_dir': 'sb', 'model_type': 'IGR_SB_Finite_Dis'},
+    4: {'model_dir': 'gs', 'model_type': 'GS_Dis'},
 }
 logger = setup_logger(log_file_name='./Log/nll.txt', logger_name='nll')
 for dataset in datasets:
@@ -26,5 +27,7 @@ for dataset in datasets:
                 current_path = path_to_trained_models + l + '/'
                 checks, weights_file = manage_files(path_to_trained_models + l + '/')
                 logger.info(current_path)
-                estimate_log_likelihood(current_path, dataset, weights_file, logger,
-                                        samples_n, v['model_type'], run_with_sample)
+                logger.info(f'Hyper present: {checks[0]}, Selected weights: {checks[1]}')
+                if not check_only:
+                    estimate_log_likelihood(current_path, dataset, weights_file, logger,
+                                            samples_n, v['model_type'], run_with_sample)
