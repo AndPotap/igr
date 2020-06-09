f1bf570d31cfa226bf72130d57301f97eb4da709
AndPotap <apotapczynski@gmail.com>
2020-06-09 14:16:56 -0400

    Updated script for Omniglot

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
-temps = [0.15]
+temps = [0.10]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 
 # import numpy as np
 # np.random.seed(21)
 # seeds = np.random.randint(low=1, high=int(1.e4), size=5)
-# seeds = [5328, 5945, 8965, 49, 9337]
-seeds = [9335]
+seeds = [5328, 5945, 8965, 49, 9337]
+# seeds = [9335]
 
 model_cases = {
     1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
@@ -28,18 +28,19 @@ model_cases = {
 }
 dataset_cases = {
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense'},
-    # 1: {'dataset_name': 'mnist', 'architecture': 'dense_nonlinear'},
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense_relax'},
     # 2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
-    2: {'dataset_name': 'fmnist', 'architecture': 'dense_relax'},
+    # 2: {'dataset_name': 'fmnist', 'architecture': 'dense_relax'},
     # 3: {'dataset_name': 'omniglot', 'architecture': 'dense'},
+    # 3: {'dataset_name': 'omniglot', 'architecture': 'dense_relax'},
+    3: {'dataset_name': 'omniglot', 'architecture': 'dense_nonlinear'},
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
-         'check_every': 1,
+         'check_every': 50,
          'sample_from_disc_kl': True,
          'test_with_one_hot': True,
-         'epochs': 25,
+         'epochs': 500,
          'learning_rate': 3 * 1.e-4,
          'batch_n': 100,
          'num_of_discrete_var': 20,
