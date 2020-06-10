8dcc3b40fa745f877f3382b96db1c3fb87e1e6ec
AndPotap <apotapczynski@gmail.com>
2020-06-10 08:10:46 -0400

    Left script ready for experiments

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
-temps = [0.10]
+temps = [0.15]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 
 # import numpy as np
@@ -12,8 +12,8 @@ seeds = [5328, 5945, 8965, 49, 9337]
 # seeds = [9335]
 
 model_cases = {
-    1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
-        'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
+    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
+    #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
@@ -24,29 +24,30 @@ model_cases = {
     # 5: {'model_type': 'GS_Dis', 'n_required': 10},
     # 6: {'model_type': 'Relax_GS_Dis', 'n_required': 10},
     # 7: {'model_type': 'Relax_Ber_Dis', 'n_required': 200},
-    # 8: {'model_type': 'Relax_IGR', 'n_required': 9},
+    8: {'model_type': 'Relax_IGR', 'n_required': 9},
 }
 dataset_cases = {
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense'},
     # 1: {'dataset_name': 'mnist', 'architecture': 'dense_relax'},
+    1: {'dataset_name': 'mnist', 'architecture': 'dense_nonlinear'},
     # 2: {'dataset_name': 'fmnist', 'architecture': 'dense'},
     # 2: {'dataset_name': 'fmnist', 'architecture': 'dense_relax'},
     # 3: {'dataset_name': 'omniglot', 'architecture': 'dense'},
     # 3: {'dataset_name': 'omniglot', 'architecture': 'dense_relax'},
-    3: {'dataset_name': 'omniglot', 'architecture': 'dense_nonlinear'},
+    # 3: {'dataset_name': 'omniglot', 'architecture': 'dense_nonlinear'},
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
-         'check_every': 50,
-         'sample_from_disc_kl': True,
-         'test_with_one_hot': True,
-         'epochs': 500,
-         'learning_rate': 3 * 1.e-4,
+         'sample_size': 4,
+         'check_every': 10,
+         'epochs': 200,
+         'learning_rate': 1 * 1.e-4,
          'batch_n': 100,
          'num_of_discrete_var': 20,
+         'sample_from_disc_kl': True,
          'stick_the_landing': True,
-         'sample_size': 1, 'sample_from_cont_kl': True,
-         'run_jv': False, 'gamma': tf.constant(30.),
+         'test_with_one_hot': True,
+         'sample_from_cont_kl': True, 'run_jv': False, 'gamma': tf.constant(30.),
          'cont_c_linspace': (0., 5., 25_000), 'disc_c_linspace': (0., 5., 25_000)}
 
 run_vae_for_all_cases(hyper, model_cases, dataset_cases, temps,
