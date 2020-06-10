aefddf83dbea146c4b016a30af70cf07f1e19315
AndPotap <apotapczynski@gmail.com>
2020-06-09 20:18:01 -0400

    Took out pass all samples of estimation ll signature

--- Utils/estimate_loglike.py
+++ Utils/estimate_loglike.py
@@ -24,12 +24,11 @@ def get_available_logs(path):
 
 
 def estimate_log_likelihood(path_to_trained_models, dataset_name, weights_file, logger,
-                            samples_n, model_type, run_with_sample, pass_all_samples=True):
+                            samples_n, model_type, run_with_sample):
     tic = time.time()
     test_dataset, hyper, epoch = load_hyper_and_data(path_to_trained_models, dataset_name,
                                                      samples_n, run_with_sample)
-    vae_opt = setup_optimizer(path_to_trained_models, hyper,
-                              model_type, weights_file, pass_all_samples)
+    vae_opt = setup_optimizer(path_to_trained_models, hyper, model_type, weights_file)
     calculate_test_log_likelihood(logger, vae_opt, test_dataset, epoch, model_type, tic)
 
 
@@ -53,12 +52,10 @@ def load_hyper(path_to_trained_models, samples_n, hyper_file='hyper.pkl'):
     return hyper
 
 
-def setup_optimizer(path_to_trained_models, hyper, model_type, weights_file='vae.h5',
-                    pass_all_samples=True):
+def setup_optimizer(path_to_trained_models, hyper, model_type, weights_file='vae.h5'):
     vae_opt = construct_nets_and_optimizer(hyper=hyper, model_type=model_type)
     vae_opt.nets.load_weights(filepath=path_to_trained_models + weights_file)
     vae_opt.test_with_one_hot = True
-    vae_opt.pass_all_samples_to_decoder = pass_all_samples
     return vae_opt
 
 
