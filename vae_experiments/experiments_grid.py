5b3fcd4b513be192c339db31b74ac3cf9b6c8b48
AndPotap <apotapczynski@gmail.com>
2020-07-01 19:26:23 -0400

    Added dtype to script

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from Models.train_vae import run_vae_for_all_cases
 
 run_with_sample = False
@@ -8,12 +9,12 @@ temps = [0.67]
 # import numpy as np
 # np.random.seed(21)
 # seeds = np.random.randint(low=1, high=int(1.e4), size=5)
-seeds = [5328, 5945, 8965, 49, 9337]
-# seeds = [9335]
+# seeds = [5328, 5945, 8965, 49, 9337]
+seeds = [9335]
 
 model_cases = {
-    #  1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
-    #      'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
+    # 1: {'model_type': 'IGR_I_Dis', 'n_required': 9,
+    #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 2: {'model_type': 'IGR_Planar_Dis', 'n_required': 9,
     #     'prior_file': './Results/mu_xi_unif_10_IGR_I.pkl'},
     # 3: {'model_type': 'IGR_SB_Finite_Dis', 'n_required': 9,
@@ -38,6 +39,7 @@ dataset_cases = {
 }
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
+         'dtype': tf.float32,
          'sample_size': 1,
          'check_every': 1,
          'epochs': 100,
