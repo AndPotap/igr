94cf67d18c7b9b30271dc6fb9fa9989142658c39
AndPotap <apotapczynski@gmail.com>
2020-08-08 22:21:37 -0400

    Set script for experiments

--- vae_experiments/experiments_grid.py
+++ vae_experiments/experiments_grid.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 from Models.train_vae import run_vae_for_all_cases
 
-run_with_sample = False
-temps = [0.67]
-# temps = [0.15]
+run_with_sample = True
+# temps = [0.67]
+temps = [0.15]
 # temps = [0.03, 0.07, 0.10, 0.15, 0.25, 0.50, 0.67]
 
 # import numpy as np
@@ -22,10 +22,10 @@ model_cases = {
     # 4: {'model_type': 'IGR_SB_Dis', 'n_required': 49,
     #     'prior_file': './Results/mu_xi_unif_50_IGR_SB_Finite.pkl',
     #     'threshold': 0.9, 'truncation_option': 'quantile'},
-    5: {'model_type': 'GS_Dis', 'n_required': 10},
+    # 5: {'model_type': 'GS_Dis', 'n_required': 10},
     # 6: {'model_type': 'Relax_GS_Dis', 'n_required': 10},
     # 7: {'model_type': 'Relax_Ber_Dis', 'n_required': 200},
-    # 8: {'model_type': 'Relax_IGR', 'n_required': 9},
+    8: {'model_type': 'Relax_IGR', 'n_required': 9},
 }
 dataset_cases = {
     1: {'dataset_name': 'mnist', 'architecture': 'dense'},
@@ -41,7 +41,7 @@ hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
          'dtype': tf.float32,
          'sample_size': 1,
-         'check_every': 1,
+         'check_every': 10,
          'epochs': 100,
          'learning_rate': 3 * 1.e-4,
          'batch_n': 100,
