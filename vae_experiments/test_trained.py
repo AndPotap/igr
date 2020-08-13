db77718b8bd62585d6794ea49b176708b1bffd1c
AndPotap <apotapczynski@gmail.com>
2020-08-13 10:30:03 -0400

    Using rebuttal folder

--- vae_experiments/test_trained.py
+++ vae_experiments/test_trained.py
@@ -5,31 +5,31 @@ from Utils.estimate_loglike import setup_logger
 
 run_with_sample = False
 check_only = False
-samples_n = 1 * int(1.e0)
+samples_n = 1 * int(1.e2)
 # datasets = ['mnist', 'fmnist', 'omniglot']
-datasets = ['fmnist', 'omniglot']
-# datasets = ['mnist']
+# datasets = ['fmnist', 'omniglot']
+datasets = ['mnist']
 # datasets = ['fmnist']
 # datasets = ['omniglot']
-# architectures = ['nonlinear']
 # architectures = ['linear']
 # architectures = ['linear_no_sl']
-architectures = ['linear_w_sl']
+# architectures = ['linear_w_sl']
+architectures = ['nonlinear']
 # architectures = ['linear_no_sl', 'linear_w_sl']
 models = {
     # 1: {'model_dir': 'igr', 'model_type': 'IGR_I_Dis'},
     # 2: {'model_dir': 'pf', 'model_type': 'IGR_Planar_Dis'},
     # 3: {'model_dir': 'sb', 'model_type': 'IGR_SB_Finite_Dis'},
-    4: {'model_dir': 'gs', 'model_type': 'GS_Dis'},
-    # 5: {'model_dir': 'relax_igr', 'model_type': 'Relax_IGR'},
+    # 4: {'model_dir': 'gs', 'model_type': 'GS_Dis'},
+    5: {'model_dir': 'relax_igr', 'model_type': 'Relax_IGR'},
     # 6: {'model_dir': 'relax_gs', 'model_type': 'Relax_GS_Dis'},
 }
 logger = setup_logger(log_file_name='./Log/nll.txt', logger_name='nll')
 for dataset in datasets:
     for arch in architectures:
         for _, v in models.items():
-            path_to_trained_models = './Results/trained_models/' + dataset + '/'
-            # path_to_trained_models = './Results/rebuttal/' + dataset + '/'
+            # path_to_trained_models = './Results/trained_models/' + dataset + '/'
+            path_to_trained_models = './Results/rebuttal/' + dataset + '/'
             path_to_trained_models += v['model_dir'] + '/'
             path_to_trained_models += arch + '/'
             logs = get_available_logs(path_to_trained_models)
