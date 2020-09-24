40511fea4265658e71e4d4a731925a9a832bc16b
AndPotap <apotapczynski@gmail.com>
2020-09-24 17:11:19 -0500

    Splitted the test cases

--- Tests/TestDLGMM.py
+++ Tests/TestDLGMM.py
@@ -55,15 +55,11 @@ class TestDLGMM(unittest.TestCase):
         x_logit = tf.random.normal(shape=(1, 4, 4, 1, n_required))
         self.hyper['n_required'] = n_required
         nets, optimizer = [], []
+
         optvae = OptDLGMM(nets, optimizer, self.hyper)
         approx = optvae.compute_log_px_z(x, x_logit, pi)
-        x_broad = tf.repeat(tf.expand_dims(x, 4), axis=4, repeats=n_required)
-        x_broad = tf.reshape(x_broad, shape=(16, 4))
-        x_logit = tf.reshape(x_logit, shape=(16, 4))
-        dist = tfpd.Bernoulli(logits=x_logit)
-        ans = dist.log_prob(x_broad)
-        ans = tf.reduce_sum(dist.log_prob(x_broad), axis=0)
-        ans = tf.reduce_sum(pi[0, :, 0, 0] * ans)
+        ans = calculate_log_px_z(x, x_logit, pi)
+
         diff = tf.linalg.norm(approx - ans) / tf.linalg.norm(ans)
         print('\nTEST: Reconstruction')
         print(f'Diff {diff:1.3e}')
@@ -79,23 +75,12 @@ class TestDLGMM(unittest.TestCase):
         self.hyper['n_required'] = n_required
         nets, optimizer = [], []
         optvae = OptDLGMM(nets, optimizer, self.hyper)
-        optvae.mu_prior = tf.zeros(shape=(batch_n, 1, sample_size, dim))
-        mult = 1. / tf.sqrt(tf.constant(dim, dtype=pi.dtype))
-        for k in range(n_required - 1):
-            mu_prior = tf.ones(shape=(batch_n, 1, sample_size, dim))
-            u = tf.random.uniform(shape=mu_prior.shape)
-            mu_prior = tf.where(u < 0.5, -1.0, 1.0)
-            mu_prior = mult * mu_prior
-            optvae.mu_prior = tf.concat([optvae.mu_prior, mu_prior],
-                                        axis=1)
+        optvae.mu_prior = create_separated_prior_means(batch_n, n_required, sample_size,
+                                                       dim, pi.dtype)
         optvae.log_var_prior = tf.zeros_like(z)
         approx = optvae.compute_log_pz(z, pi)
+        ans = calculate_log_pz(z, pi, optvae.mu_prior, optvae.log_var_prior)
 
-        loc = optvae.mu_prior
-        scale = tf.math.exp(0.5 * optvae.log_var_prior)
-        dist = tfpd.Normal(loc, scale)
-        ans = pi * tf.reduce_sum(dist.log_prob(z), axis=3, keepdims=True)
-        ans = tf.reduce_mean(tf.reduce_sum(ans, axis=(1, 2, 3)))
         diff = tf.linalg.norm(approx - ans) / tf.linalg.norm(ans)
         print('\nTEST: Normal Prior')
         print(f'Diff {diff:1.3e}')
@@ -107,17 +92,12 @@ class TestDLGMM(unittest.TestCase):
         log_a = tf.expand_dims(tf.expand_dims(log_a, axis=-1), axis=-1)
         log_b = tf.constant([[0., -1., 1., 2.0, -2.0]])
         log_b = tf.expand_dims(tf.expand_dims(log_b, axis=-1), axis=-1)
-        a, b = tf.math.exp(log_a), tf.math.exp(log_b)
-        n_required = log_a.shape[1]
-        self.hyper['n_required'] = n_required
+        self.hyper['n_required'] = log_a.shape[1]
         nets, optimizer = [], []
         optvae = OptDLGMM(nets, optimizer, self.hyper)
-        ans = 0.
-        for k in range(n_required):
-            dist = tfpd.Kumaraswamy(concentration0=a[:, k, 0, 0],
-                                    concentration1=b[:, k, 0, 0])
-            ans += dist.entropy()
+
         approx = optvae.compute_kld(log_a, log_b)
+        ans = calculate_kumar_entropy(log_a, log_b)
         diff = tf.linalg.norm(approx - ans) / tf.linalg.norm(ans)
         print('\nTEST: Kumaraswamy KL')
         print(f'Diff {diff:1.3e}')
@@ -136,19 +116,68 @@ class TestDLGMM(unittest.TestCase):
         mean = tf.random.normal(shape=(batch_n, n_required, sample_size, dim))
         log_var = tf.zeros_like(z)
         approx = optvae.compute_log_qz_x(z, pi, mean, log_var)
+        ans = calculate_log_qz_x(z, pi, mean, log_var)
 
-        ans = 0
-        for k in range(n_required):
-            loc = mean[:, k, 0, :]
-            scale = tf.math.exp(0.5 * log_var[:, k, 0, :])
-            dist = tfpd.Normal(loc=loc, scale=scale)
-            aux = tf.reduce_prod(tf.math.exp(dist.log_prob(z[:, k, 0, :])), axis=1)
-            ans += pi[0, k, 0, 0] * aux
-        ans = tf.reduce_mean(tf.math.log(ans))
         diff = tf.linalg.norm(approx - ans) / tf.linalg.norm(ans)
         print('\nTEST: Normal A Posterior')
         print(f'Diff {diff:1.3e}')
         self.assertTrue(expr=diff < test_tolerance)
+
+
+def calculate_log_px_z(x, x_logit, pi):
+    x_broad = tf.repeat(tf.expand_dims(x, 4), axis=4, repeats=pi.shape[1])
+    x_broad = tf.reshape(x_broad, shape=(16, 4))
+    x_logit = tf.reshape(x_logit, shape=(16, 4))
+    dist = tfpd.Bernoulli(logits=x_logit)
+    log_px_z = dist.log_prob(x_broad)
+    log_px_z = tf.reduce_sum(dist.log_prob(x_broad), axis=0)
+    log_px_z = tf.reduce_sum(pi[0, :, 0, 0] * log_px_z)
+    return log_px_z
+
+
+def calculate_log_pz(z, pi, mu_prior, log_var_prior):
+    loc = mu_prior
+    scale = tf.math.exp(0.5 * log_var_prior)
+    dist = tfpd.Normal(loc, scale)
+    log_pz = pi * tf.reduce_sum(dist.log_prob(z), axis=3, keepdims=True)
+    log_pz = tf.reduce_mean(tf.reduce_sum(log_pz, axis=(1, 2, 3)))
+    return log_pz
+
+
+def calculate_kumar_entropy(log_a, log_b):
+    a, b = tf.math.exp(log_a), tf.math.exp(log_b)
+    n_required = log_a.shape[1]
+    ans = 0.
+    for k in range(n_required):
+        dist = tfpd.Kumaraswamy(concentration0=a[:, k, 0, 0],
+                                concentration1=b[:, k, 0, 0])
+        ans += dist.entropy()
+    return ans
+
+
+def calculate_log_qz_x(z, pi, mean, log_var):
+    n_required = pi.shape[1]
+    qz_x = 0
+    for k in range(n_required):
+        loc = mean[:, k, 0, :]
+        scale = tf.math.exp(0.5 * log_var[:, k, 0, :])
+        dist = tfpd.Normal(loc=loc, scale=scale)
+        aux = tf.reduce_prod(tf.math.exp(dist.log_prob(z[:, k, 0, :])), axis=1)
+        qz_x += pi[0, k, 0, 0] * aux
+        log_qz_x = tf.reduce_mean(tf.math.log(qz_x))
+    return log_qz_x
+
+
+def create_separated_prior_means(batch_n, n_required, sample_size, dim, dtype):
+    mu_prior = tf.zeros(shape=(batch_n, 1, sample_size, dim))
+    mult = 1. / tf.sqrt(tf.constant(dim, dtype=dtype))
+    for k in range(n_required - 1):
+        mu = tf.ones(shape=(batch_n, 1, sample_size, dim))
+        u = tf.random.uniform(shape=mu.shape)
+        mu = tf.where(u < 0.5, -1.0, 1.0)
+        mu = mult * mu
+        mu_prior = tf.concat([mu_prior, mu], axis=1)
+    return mu
 
 
 if __name__ == '__main__':
