203a27c29a6452703ae49d6acab3864df5fe7299
AndPotap <apotapczynski@gmail.com>
2020-10-07 12:23:32 -0500

    Set parameters to baseline

--- vae_experiments/dlgmm.py
+++ vae_experiments/dlgmm.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from Models.train_vae import run_vae
 
-run_with_sample = True
+run_with_sample = False
 # seeds = [5328, 5945, 8965, 49, 9337]
 hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'temp': 1.0, 'sample_from_disc_kl': True, 'stick_the_landing': True,
@@ -9,14 +9,15 @@ hyper = {'latent_norm_n': 0, 'num_of_norm_param': 0, 'num_of_norm_var': 0,
          'sample_size_testing': 1 * int(1.e0),
          'dataset_name': 'mnist',
          'seed': 9335,
-         'model_type': 'DLGMM_IGR_SB',
+         'model_type': 'DLGMM',
+         # 'model_type': 'DLGMM_IGR_SB',
          # 'model_type': 'DLGMM_IGR',
          'architecture': 'dlgmm_dense',
          'batch_n': 100,
-         'n_required': 50,
-         'latent_discrete_n': 50,
-         # 'n_required': 5,
-         # 'latent_discrete_n': 5,
+         # 'n_required': 50,
+         # 'latent_discrete_n': 50,
+         'n_required': 5,
+         'latent_discrete_n': 5,
          'num_of_discrete_param': 4,
          'sample_size': 1,
          'num_of_discrete_var': 50,
