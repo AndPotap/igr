49c23c817a51d38d9079dbb25b6b41f7dac513f1
AndPotap <apotapczynski@gmail.com>
2020-10-21 07:05:47 -0500

    Modified script to compute nll

--- vae_experiments/test_trained.py
+++ vae_experiments/test_trained.py
@@ -8,34 +8,34 @@ check_only = False
 samples_n = 1 * int(1.e2)
 # datasets = ['mnist', 'fmnist', 'omniglot']
 # datasets = ['fmnist', 'omniglot']
-datasets = ['mnist']
+# datasets = ['mnist']
 # datasets = ['fmnist']
-# datasets = ['omniglot']
-# architectures = ['linear']
+datasets = ['omniglot']
 # architectures = ['linear_no_sl']
 # architectures = ['linear_w_sl']
-architectures = ['nonlinear']
-# architectures = ['linear_no_sl', 'linear_w_sl']
+# architectures = ['nonlinear_wo_sl']
+# architectures = ['nonlinear_w_sl']
+architectures = ['nonlinear_w_sl', 'nonlinear_wo_sl']
 models = {
     # 1: {'model_dir': 'igr', 'model_type': 'IGR_I_Dis'},
     # 2: {'model_dir': 'pf', 'model_type': 'IGR_Planar_Dis'},
     # 3: {'model_dir': 'sb', 'model_type': 'IGR_SB_Finite_Dis'},
-    # 4: {'model_dir': 'gs', 'model_type': 'GS_Dis'},
-    5: {'model_dir': 'relax_igr', 'model_type': 'Relax_IGR'},
+    4: {'model_dir': 'gs', 'model_type': 'GS_Dis'},
+    # 5: {'model_dir': 'relax_igr', 'model_type': 'Relax_IGR'},
     # 6: {'model_dir': 'relax_gs', 'model_type': 'Relax_GS_Dis'},
 }
 logger = setup_logger(log_file_name='./Log/nll.txt', logger_name='nll')
 for dataset in datasets:
     for arch in architectures:
         for _, v in models.items():
-            # path_to_trained_models = './Results/trained_models/' + dataset + '/'
-            path_to_trained_models = './Results/rebuttal/' + dataset + '/'
+            path_to_trained_models = './Results/trained_models/' + dataset + '/'
+            # path_to_trained_models = './Results/rebuttal/' + dataset + '/'
             path_to_trained_models += v['model_dir'] + '/'
             path_to_trained_models += arch + '/'
             logs = get_available_logs(path_to_trained_models)
-            for l in logs:
-                current_path = path_to_trained_models + l + '/'
-                checks, weights_file = manage_files(path_to_trained_models + l + '/')
+            for log in logs:
+                current_path = path_to_trained_models + log + '/'
+                checks, weights_file = manage_files(path_to_trained_models + log + '/')
                 logger.info(current_path)
                 logger.info(f'Hyper present: {checks[0]}, Selected weights: {checks[1]}')
                 if not check_only:
